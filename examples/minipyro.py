7ba09eb7294caea13686f1caee6f49d02d8ba8d2
Eli <eli.bingham@uber.com>
2019-02-02 14:08:02 -0800

    Add minipyro example

---
+++ examples/minipyro.py
@@ -0,0 +1,306 @@
+"""
+Mini Pyro
+---------
+
+This file contains a minimal implementation of the Pyro Probabilistic
+Programming Language. The API (method signatures, etc.) match that of
+the full implementation as closely as possible. This file is independent
+of the rest of Pyro, with the exception of the :mod:`pyro.distributions`
+module.
+
+An accompanying example that makes use of this implementation can be
+found at examples/minipyro.py.
+"""
+from __future__ import absolute_import, division, print_function
+
+from collections import OrderedDict
+
+import torch
+
+import funsor
+
+# Pyro keeps track of two kinds of global state:
+# i)  The effect handler stack, which enables non-standard interpretations of
+#     Pyro primitives like sample();
+#     See http://docs.pyro.ai/en/0.3.0-release/poutine.html
+# ii) Trainable parameters in the Pyro ParamStore;
+#     See http://docs.pyro.ai/en/0.3.0-release/parameters.html
+
+PYRO_STACK = []
+PARAM_STORE = {}
+
+
+def get_param_store():
+    return PARAM_STORE
+
+
+# The base effect handler class (called Messenger here for consistency with Pyro).
+class Messenger(object):
+    def __init__(self, fn=None):
+        self.fn = fn
+
+    # Effect handlers push themselves onto the PYRO_STACK.
+    # Handlers earlier in the PYRO_STACK are applied first.
+    def __enter__(self):
+        PYRO_STACK.append(self)
+
+    def __exit__(self, *args, **kwargs):
+        assert PYRO_STACK[-1] is self
+        PYRO_STACK.pop()
+
+    def process_message(self, msg):
+        pass
+
+    def postprocess_message(self, msg):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        with self:
+            return self.fn(*args, **kwargs)
+
+
+# A first useful example of an effect handler.
+# trace records the inputs and outputs of any primitive site it encloses,
+# and returns a dictionary containing that data to the user.
+class trace(Messenger):
+    def __enter__(self):
+        super(trace, self).__enter__()
+        self.trace = OrderedDict()
+        return self.trace
+
+    # trace illustrates why we need postprocess_message in addition to process_message:
+    # We only want to record a value after all other effects have been applied
+    def postprocess_message(self, msg):
+        assert msg["name"] not in self.trace, "all sites must have unique names"
+        self.trace[msg["name"]] = msg.copy()
+
+    def get_trace(self, *args, **kwargs):
+        self(*args, **kwargs)
+        return self.trace
+
+
+# A second example of an effect handler for setting the value at a sample site.
+# This illustrates why effect handlers are a useful PPL implementation technique:
+# We can compose trace and replay to replace values but preserve distributions,
+# allowing us to compute the joint probability density of samples under a model.
+# See the definition of elbo(...) below for an example of this pattern.
+class replay(Messenger):
+    def __init__(self, fn, guide_trace):
+        self.guide_trace = guide_trace
+        super(replay, self).__init__(fn)
+
+    def process_message(self, msg):
+        if msg["name"] in self.guide_trace:
+            msg["value"] = self.guide_trace[msg["name"]]["value"]
+
+
+# block allows the selective application of effect handlers to different parts of a model.
+# Sites hidden by block will only have the handlers below block on the PYRO_STACK applied,
+# allowing inference or other effectful computations to be nested inside models.
+class block(Messenger):
+    def __init__(self, fn=None, hide_fn=lambda msg: True):
+        self.hide_fn = hide_fn
+        super(block, self).__init__(fn)
+
+    def process_message(self, msg):
+        if self.hide_fn(msg):
+            msg["stop"] = True
+
+
+# This limited implementation of PlateMessenger only implements broadcasting.
+class plate(Messenger):
+    def __init__(self, fn, size, dim, name):
+        assert dim < 0
+        self.size = size
+        self.dim = dim
+        super(PlateMessenger, self).__init__(fn)
+
+    def process_message(self, msg):
+        if msg["type"] == "sample":
+            batch_shape = msg["fn"].batch_shape
+            if len(batch_shape) < -self.dim or batch_shape[self.dim] != self.size:
+                batch_shape = [1] * (-self.dim - len(batch_shape)) + list(batch_shape)
+                batch_shape[self.dim] = self.size
+                msg["fn"] = msg["fn"].expand(torch.Size(batch_shape))
+
+    def __iter__(self):
+        return range(self.size)
+
+
+# apply_stack is called by pyro.sample and pyro.param.
+# It is responsible for applying each Messenger to each effectful operation.
+def apply_stack(msg):
+    for pointer, handler in enumerate(reversed(PYRO_STACK)):
+        handler.process_message(msg)
+        # When a Messenger sets the "stop" field of a message,
+        # it prevents any Messengers above it on the stack from being applied.
+        if msg.get("stop"):
+            break
+    if msg["value"] is None:
+        msg["value"] = msg["fn"](*msg["args"])
+
+    # A Messenger that sets msg["stop"] == True also prevents application
+    # of postprocess_message by Messengers above it on the stack
+    # via the pointer variable from the process_message loop
+    for handler in PYRO_STACK[-pointer-1:]:
+        handler.postprocess_message(msg)
+    return msg
+
+
+# sample is an effectful version of Distribution.sample(...)
+# When any effect handlers are active, it constructs an initial message and calls apply_stack.
+def sample(fn, obs=None, name=None):
+
+    # if there are no active Messengers, we just draw a sample and return it as expected:
+    if not PYRO_STACK:
+        return fn()
+
+    # Otherwise, we initialize a message...
+    initial_msg = {
+        "type": "sample",
+        "name": name if name is not None else "sample",
+        "fn": fn,
+        "args": (),
+        "value": obs,
+    }
+
+    # ...and use apply_stack to send it to the Messengers
+    msg = apply_stack(initial_msg)
+    return msg["value"]
+
+
+# param is an effectful version of PARAM_STORE.setdefault
+# When any effect handlers are active, it constructs an initial message and calls apply_stack.
+def param(init_value=None, name=None):
+
+    if init_value is None and name is None:
+        raise ValueError("empty args to param")
+
+    def fn(init_value):
+        value = PARAM_STORE.setdefault(name, init_value)
+        value.requires_grad_()
+        return value
+
+    # if there are no active Messengers, we just draw a sample and return it as expected:
+    if not PYRO_STACK:
+        return fn(init_value)
+
+    # Otherwise, we initialize a message...
+    initial_msg = {
+        "type": "param",
+        "name": name,
+        "fn": fn,
+        "args": (init_value,),
+        "value": None,
+    }
+
+    # ...and use apply_stack to send it to the Messengers
+    msg = apply_stack(initial_msg)
+    return msg["value"]
+
+
+# copied from example.py; this is like pyro.sample, but uses args_ rather than poutine
+def pyro_sample(name, fn, obs=None):
+    if obs is not None:
+        value = obs
+        log_prob = fn(value=obs)
+    elif args_.eager:
+        # this is eager like usual pyro.sample
+        args, log_prob = fn.sample(['value'])
+        value = args['value']
+    else:
+        # this is deferred like pyro.sample during enumeration
+        value = funsor.var(name, fn.schema['value'])
+        log_prob = fn(value=value)
+    return value, log_prob
+
+
+class deferred(Messenger):
+
+    def process_message(self, msg):
+        if msg["type"] == "sample":
+            msg["value"] = funsor.var(name, fn.schema["value"])
+        return msg
+
+
+def main(args):
+    """
+    minipyro version of Gaussian HMM example
+    """
+
+    # a Gaussian HMM
+    def model(data):
+
+        trans_noise = param(name="trans_noise")
+        emit_noise = param(name="emit_noise")
+
+        x_curr = 0.
+        for t, y in enumerate(data):
+            x_prev = x_curr
+
+            # a sample statement
+            x_curr = sample(
+                funsor.Normal(loc=x_prev, scale=trans_noise),
+                name='x_{}'.format(t))
+
+            # an observe statement
+            y_curr = sample(
+                funsor.Normal(loc=x_curr, scale=emit_noise),
+                obs=y,
+                name='y_{}'.format(t))
+
+        return x_curr
+
+    trans_noise = param(torch.tensor(0.1, requires_grad=True), name="trans_noise")
+    emit_noise = param(torch.tensor(0.5, requires_grad=True), name="emit_noise")
+    data = torch.randn(args_.time_steps)
+
+    params = [trans_noise, emit_noise]
+
+    # training loop
+    print('---- training ----')
+    optim = torch.optim.Adam(params, lr=args_.learning_rate)
+    for step in range(args_.train_steps):
+        optim.zero_grad()
+
+        tr = trace(deferred(model)).get_trace(data)
+
+        log_probs = [node["fn"](value=node["value"])
+                     for node in tr.values()
+                     if node["type"] == "sample"]
+
+        loss = -funsor.logsumproductexp(*log_probs)  # contracts
+
+        if step % 10 == 0:
+            print('step {} loss = {}'.format(step, loss.item()))
+        loss.backward()
+        optim.step()
+
+    # serving by drawing a posterior sample
+    print('---- serving ----')
+    eager_args, log_probs = model(data)
+    lazy_args, log_prob = funsor.logsumproductexp(log_probs)
+    joint_sample = eager_args
+    joint_sample.update(lazy_args)
+    for key, value in sorted(joint_sample.items()):
+        print('{} = {}'.format(key, value.item()))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Gaussian HMM example")
+    parser.add_argument("-t", "--time-steps", default=10, type=int)
+    parser.add_argument("-n", "--train-steps", default=101, type=int)
+    parser.add_argument("-lr", "--learning-rate", default=0.05, type=float)
+    parser.add_argument("--eager", action='store_true')
+    parser.add_argument("--filter", action='store_true')
+    parser.add_argument("--xfail-if-not-implemented", action='store_true')
+    args = parser.parse_args()
+
+    if args.xfail_if_not_implemented:
+        try:
+            main(args)
+        except NotImplementedError:
+            print('XFAIL example.py')
+    else:
+        main(args)
