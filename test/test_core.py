813f875629056e49769a2662f0e55446099a2841
Fritz Obermeyer <fritzo@uber.com>
2019-02-02 14:34:08 -0800

    Split core.py out of __init__.py

--- test/test_core.py
+++ test/test_core.py
@@ -6,6 +6,7 @@ import pytest
 import torch
 
 import funsor
+from funsor.core import align_tensors
 
 
 def check_funsor(x, dims, shape, data=None):
@@ -217,8 +218,7 @@ def test_binary_funsor_funsor(symbol, dims1, dims2):
     if symbol in BOOLEAN_OPS:
         data1 = data1.byte()
         data2 = data2.byte()
-    dims, aligned = funsor._align_tensors((dims1, data1),
-                                          (dims2, data2))
+    dims, aligned = align_tensors((dims1, data1), (dims2, data2))
     expected_data = binary_eval(symbol, aligned[0], aligned[1])
 
     x1 = funsor.Tensor(dims1, data1)
