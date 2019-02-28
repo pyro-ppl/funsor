4638d591941f79371c1064da345bc1e16322b085
Fritz Obermeyer <fritzo@uber.com>
2019-02-28 12:13:04 -0800

    Implement marginalization along a dimension; add smoke test

--- funsor/gaussian.py
+++ funsor/gaussian.py
@@ -22,8 +22,12 @@ def _issubshape(subshape, supershape):
 
 
 def _scale_tril_to_precision(scale_tril):
-    scale_tril_inv = torch.inv(scale_tril)
+    scale_tril_inv = torch.inverse(scale_tril)
     return torch.matmul(scale_tril_inv.transpose(-1, -2), scale_tril_inv)
+
+
+def _mv(mat, vec):
+    return torch.matmul(mat, vec.unsqueeze(-1)).squeeze(-1)
 
 
 def to_affine(x):
@@ -71,6 +75,7 @@ class Gaussian(Funsor):
 
         # Compute total dimension of all real inputs.
         dim = sum(d.num_elements for d in inputs.values() if d.dtype == 'real')
+        assert dim
         assert loc.dim() >= 1 and loc.size(-1) == dim
         assert scale_tril.dim() >= 2 and scale_tril.shape[-2:] == (dim, dim)
 
@@ -99,7 +104,29 @@ class Gaussian(Funsor):
 
     def eager_reduce(self, op, reduced_vars):
         if op is ops.logaddexp:
-            raise NotImplementedError('TODO marginalize a real variable')
+            # Marginalize out real variables.
+            real_vars = frozenset(k for k, d in self.inputs.items() if d.dtype == "real")
+            if reduced_vars.isdisjoint(real_vars):
+                return None  # defer to default implementation
+            inputs = OrderedDict((k, d) for k, d in self.inputs.items() if k not in real_vars)
+            log_density = self.log_density
+            if real_vars <= reduced_vars:
+                result = Tensor(log_density, inputs)
+            else:
+                mask = []
+                for k, d in self.inputs.items():
+                    if d.dtype == 'real':
+                        mask.extend([k not in reduced_vars] * d.num_elements)
+                index = torch.tensor([i for i, m in enumerate(mask) if m])
+
+                loc = self.loc[..., index]
+                self_covariance = torch.matmul(self.scale_tril,
+                                               self.scale_tril.transpose(-1, -2))
+                covariance = self_covariance[..., index.unsqueeze(-1), index]
+                scale_tril = torch.cholesky(covariance)
+                result = Gaussian(log_density, loc, scale_tril, inputs)
+            return result.reduce(ops.logaddexp, reduced_vars - real_vars)
+
         elif op is ops.add:
             raise NotImplementedError('TODO product-reduce along a plate dimension')
 
@@ -110,7 +137,7 @@ class Gaussian(Funsor):
 def eager_binary_gaussian_number(op, lhs, rhs):
     if op is ops.add or op is ops.sub:
         log_density = op(lhs.log_density, rhs.data)
-        return Gaussian(log_density, lhs.loc, lhs.scale_tril)
+        return Gaussian(log_density, lhs.loc, lhs.scale_tril, lhs.inputs)
 
     return None  # defer to default implementation
 
@@ -119,7 +146,7 @@ def eager_binary_gaussian_number(op, lhs, rhs):
 def eager_binary_number_gaussian(op, lhs, rhs):
     if op is ops.add:
         log_density = op(lhs.data, rhs.log_density)
-        return Gaussian(log_density, rhs.loc, rhs.scale_tril)
+        return Gaussian(log_density, rhs.loc, rhs.scale_tril, rhs.inputs)
 
     return None  # defer to default implementation
 
@@ -127,12 +154,15 @@ def eager_binary_number_gaussian(op, lhs, rhs):
 @eager.register(Binary, object, Gaussian, Tensor)
 def eager_binary_gaussian_tensor(op, lhs, rhs):
     if op is ops.add or op is ops.sub:
+        nonreal_inputs = OrderedDict((k, d) for k, d in lhs.inputs.items()
+                                     if d.dtype != 'real')
         inputs, (rhs_data, log_density, loc, scale_tril) = align_tensors(
-                rhs.data,
-                Tensor(lhs.log_density, lhs.inputs),
-                Tensor(lhs.loc, lhs.inputs),
-                Tensor(lhs.scale_tril, lhs.inputs))
+                rhs,
+                Tensor(lhs.log_density, nonreal_inputs),
+                Tensor(lhs.loc, nonreal_inputs),
+                Tensor(lhs.scale_tril, nonreal_inputs))
         log_density = op(log_density, rhs_data)
+        inputs.update(lhs.inputs)
         return Gaussian(log_density, loc, scale_tril, inputs)
 
     return None  # defer to default implementation
@@ -141,12 +171,15 @@ def eager_binary_gaussian_tensor(op, lhs, rhs):
 @eager.register(Binary, object, Tensor, Gaussian)
 def eager_binary_tensor_gaussian(op, lhs, rhs):
     if op is ops.add:
+        nonreal_inputs = OrderedDict((k, d) for k, d in rhs.inputs.items()
+                                     if d.dtype != 'real')
         inputs, (lhs_data, log_density, loc, scale_tril) = align_tensors(
-                lhs.data,
-                Tensor(rhs.log_density, lhs.inputs),
-                Tensor(rhs.loc, lhs.inputs),
-                Tensor(rhs.scale_tril, lhs.inputs))
+                lhs,
+                Tensor(rhs.log_density, nonreal_inputs),
+                Tensor(rhs.loc, nonreal_inputs),
+                Tensor(rhs.scale_tril, nonreal_inputs))
         log_density = op(lhs_data, log_density)
+        inputs.update(rhs.inputs)
         return Gaussian(log_density, loc, scale_tril, inputs)
 
     return None  # defer to default implementation
@@ -165,12 +198,10 @@ def eager_binary_gaussian_gaussian(op, lhs, rhs):
         precision = lhs_precision + rhs_precision
         scale_tril_inv = torch.cholesky(precision)
         scale_tril = torch.inverse(scale_tril_inv)
-        loc = torch.matmul(scale_tril,
-                           torch.matmul(scale_tril.transpose(-1, 2),
-                                        torch.matmul(lhs_precision, lhs.loc) +
-                                        torch.matmul(rhs_precision, rhs.loc)))
+        precision_loc = _mv(lhs_precision, lhs.loc) + _mv(rhs_precision, rhs.loc)
+        loc = _mv(scale_tril, _mv(scale_tril.transpose(-1, 2), precision_loc))
         log_density = (lhs.log_density + rhs.log_density +  # FIXME add missing terms
-                       _batch_mahalanobis(scale_tril, lhs.loc, rhs.loc))
+                       _batch_mahalanobis(scale_tril, lhs.loc - rhs.loc))
         return Gaussian(log_density, loc, scale_tril, inputs)
 
     return None  # defer to default implementation
--- test/test_gaussian.py
+++ test/test_gaussian.py
@@ -2,13 +2,23 @@ from __future__ import absolute_import, division, print_function
 
 from collections import OrderedDict
 
+import pytest
 import torch
 
 from funsor.domains import bint, reals
 from funsor.gaussian import Gaussian
+from funsor.torch import Tensor
 
 
-def test_smoke():
+@pytest.mark.parametrize('expr,expected_type', [
+    ('g.logsumexp("x")', Tensor),
+    ('g + 1', Gaussian),
+    ('1 + g', Gaussian),
+    ('g + shift', Gaussian),
+    ('shift + g', Gaussian),
+    ('g + g', Gaussian),
+])
+def test_smoke(expr, expected_type):
     log_density = torch.tensor([0.0, 1.0])
     loc = torch.tensor([[0.0, 0.1, 0.2],
                         [2.0, 3.0, 4.0]])
@@ -19,4 +29,10 @@ def test_smoke():
                                 [0.1, 1.0, 0.0],
                                 [0.2, 0.3, 1.0]]])
     inputs = OrderedDict([('i', bint(2)), ('x', reals(3))])
-    Gaussian(log_density, loc, scale_tril, inputs)
+    g = Gaussian(log_density, loc, scale_tril, inputs)
+    assert isinstance(g, Gaussian)
+    shift = Tensor(torch.tensor([-1., 1.]), OrderedDict([('i', bint(2))]))
+    assert isinstance(shift, Tensor)
+
+    result = eval(expr)
+    assert isinstance(result, expected_type)
