9160416558e98896fedeb9f0fea9c7ff5eb544ef
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2019-02-28 20:22:55 -0800

    Add simple wrapper around torch.einsum (#41)

--- funsor/__init__.py
+++ funsor/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
-from funsor.domains import Domain, find_domain, bint, reals
+from funsor.domains import Domain, bint, find_domain, reals
 from funsor.interpreter import reinterpret
 from funsor.terms import Funsor, Number, Variable, of_shape, to_funsor
-from funsor.torch import Function, Tensor, arange, function
+from funsor.torch import Function, Tensor, arange, einsum, function
 
 from . import distributions, domains, handlers, interpreter, minipyro, ops, terms, torch
 
@@ -18,6 +18,7 @@ __all__ = [
     'backward',
     'distributions',
     'domains',
+    'einsum',
     'find_domain',
     'function',
     'handlers',
