cc92b02a019b4b43554370c91243baaf914b208e
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2019-03-01 16:31:30 -0800

    Fix filling in of defaults for distribution classes (#44)

--- funsor/distributions.py
+++ funsor/distributions.py
@@ -64,6 +64,12 @@ class CategoricalMeta(FunsorMeta):
     Wrapper to fill in default params.
     """
     def __call__(cls, probs, value=None):
+        probs = to_funsor(probs)
+        if value is None:
+            size = probs.output.shape[0]
+            value = Variable('value', bint(size))
+        else:
+            value = to_funsor(value)
         return super(CategoricalMeta, cls).__call__(probs, value)
 
 
@@ -72,12 +78,6 @@ class Categorical(Distribution):
     dist_class = dist.Categorical
 
     def __init__(self, probs, value=None):
-        probs = to_funsor(probs)
-        if value is None:
-            size = probs.output.shape[0]
-            value = Variable('value', bint(size))
-        else:
-            value = to_funsor(value)
         super(Categorical, self).__init__(probs, value)
 
 
@@ -97,16 +97,25 @@ def eager_categorical(probs, value):
     return Categorical.eager_log_prob(probs=probs, value=value)
 
 
-class Normal(Distribution):
-    dist_class = dist.Normal
-
-    def __init__(self, loc, scale, value=None):
+class NormalMeta(FunsorMeta):
+    """
+    Wrapper to fill in default params.
+    """
+    def __call__(cls, loc, scale, value=None):
         loc = to_funsor(loc)
         scale = to_funsor(scale)
         if value is None:
             value = Variable('value', reals())
         else:
             value = to_funsor(value)
+        return super(NormalMeta, cls).__call__(loc, scale, value)
+
+
+@add_metaclass(NormalMeta)
+class Normal(Distribution):
+    dist_class = dist.Normal
+
+    def __init__(self, loc, scale, value=None):
         super(Normal, self).__init__(loc, scale, value)
 
 
