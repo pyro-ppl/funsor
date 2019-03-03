d33341808f795011796f63be013c0bbdb0c96df0
eb8680 <eb8680@users.noreply.github.com>
2019-03-03 10:35:23 -0800

    Reinstate opt_einsum-based optimizer (#40)

---
+++ funsor/optimizer.py
@@ -0,0 +1,224 @@
+from __future__ import absolute_import, division, print_function
+
+import collections
+from six.moves import reduce
+
+from opt_einsum.paths import greedy
+
+from funsor.domains import find_domain
+from funsor.interpreter import interpretation, reinterpret
+from funsor.ops import ASSOCIATIVE_OPS, DISTRIBUTIVE_OPS
+from funsor.registry import KeyedRegistry
+from funsor.terms import eager, reflect, Binary, Funsor, Reduce
+
+
+class Finitary(Funsor):
+    """
+    Lazy finitary operation. Used internally in the optimizer.
+    Finitary(op, operands) == six.moves.reduce(op, operands)
+    """
+    def __init__(self, op, operands):
+        assert callable(op)
+        assert isinstance(operands, tuple)
+        assert all(isinstance(operand, Funsor) for operand in operands)
+        inputs = collections.OrderedDict()
+        for operand in operands:
+            inputs.update(operand.inputs)
+
+        output = reduce(lambda lhs, rhs: find_domain(op, lhs, rhs),
+                        [operand.output for operand in reversed(operands)])
+
+        super(Finitary, self).__init__(inputs, output)
+        self.op = op
+        self.operands = operands
+
+    def __repr__(self):
+        return 'Finitary({}, {})'.format(self.op.__name__, self.operands)
+
+    def eager_subs(self, subs):
+        if not any(k in self.inputs for k, v in subs):
+            return self
+        operands = tuple(operand.eager_subs(subs) for operand in self.operands)
+        return Finitary(self.op, operands)
+
+
+@eager.register(Finitary, object, tuple)
+def eager_finitary(op, operands):
+    return reduce(lambda lhs, rhs: Binary(op, lhs, rhs), operands)
+
+
+def associate(cls, *args):
+    result = _associate(cls, *args)
+    if result is None:
+        result = reflect(cls, *args)
+    return result
+
+
+_associate = KeyedRegistry(default=lambda *args: None)
+associate.register = _associate.register
+
+
+@associate.register(Binary, object, Funsor, Funsor)
+def binary_to_finitary(op, lhs, rhs):
+    """convert Binary to Finitary"""
+    return Finitary(op, (lhs, rhs))
+
+
+@associate.register(Finitary, object, tuple)
+def associate_finitary(op, operands):
+    # Finitary(Finitary) -> Finitary
+    new_operands = []
+    for term in operands:
+        if isinstance(term, Finitary) and term.op is op and op in ASSOCIATIVE_OPS:
+            new_operands.extend(term.operands)
+        else:
+            new_operands.append(term)
+
+    with interpretation(reflect):
+        return Finitary(op, tuple(new_operands))
+
+
+@associate.register(Reduce, object, Reduce, frozenset)
+def associate_reduce(op, arg, reduced_vars):
+    """
+    Rewrite to the largest possible Reduce(Finitary) by combining Reduces
+    Assumes that all input Reduce/Finitary ops have been rewritten
+    """
+    if arg.op is op and op in ASSOCIATIVE_OPS:
+        # Reduce(Reduce) -> Reduce
+        new_reduced_vars = reduced_vars.union(arg.reduced_vars)
+        return Reduce(op, arg.arg, new_reduced_vars)
+    return None
+
+
+def distribute(cls, *args):
+    result = _distribute(cls, *args)
+    if result is None:
+        result = reflect(cls, *args)
+    return result
+
+
+_distribute = KeyedRegistry(default=lambda *args: None)
+distribute.register = _distribute.register
+
+
+@distribute.register(Finitary, object, tuple)
+def distribute_finitary(op, operands):
+    # TODO raise an error or warning on name collision
+    if len(operands) == 1:
+        return operands[0]
+
+    reduce_op = None
+    reduce_terms, remaining_terms, reduced_vars = [], [], frozenset()
+    for term in operands:
+        # term is not reduce -> do nothing
+        # term is reduce but does not distribute -> do nothing
+        # term is reduce and distributes -> put into reduce_terms
+        if isinstance(term, Reduce):
+            if not reduce_op or not (reduce_op, op) in DISTRIBUTIVE_OPS:
+                reduce_op = term.op
+            if term.op == reduce_op and (reduce_op, op) in DISTRIBUTIVE_OPS:
+                reduce_terms.append(term)
+                reduced_vars = reduced_vars | term.reduced_vars
+            else:
+                remaining_terms.append(term)
+        else:
+            remaining_terms.append(term)
+
+    if len(reduce_terms) > 1:
+        new_finitary_term = Finitary(op, tuple(term.arg for term in reduce_terms))
+        remaining_terms.append(Reduce(reduce_op, new_finitary_term, reduced_vars))
+        return Finitary(op, tuple(remaining_terms))
+
+    return None
+
+
+def optimize(cls, *args):
+    result = _optimize(cls, *args)
+    if result is None:
+        result = reflect(cls, *args)
+    return result
+
+
+_optimize = KeyedRegistry(default=lambda *args: None)
+optimize.register = _optimize.register
+
+
+# TODO set a better value for this
+REAL_SIZE = 3  # the "size" of a real-valued dimension passed to the path optimizer
+
+
+@optimize.register(Reduce, object, Finitary, frozenset)
+def optimize_reduction(op, arg, reduced_vars):
+    r"""
+    Recursively convert large Reduce(Finitary) ops to many smaller versions
+    by reordering execution with a modified opt_einsum optimizer
+    """
+    if not (op, arg.op) in DISTRIBUTIVE_OPS:
+        return None
+
+    # build opt_einsum optimizer IR
+    inputs = []
+    size_dict = {}
+    for operand in arg.operands:
+        inputs.append(frozenset(d for d in operand.inputs.keys()))
+        size_dict.update({k: ((REAL_SIZE * v.num_elements) if v.dtype == 'real' else v.dtype)
+                          for k, v in operand.inputs.items()})
+    outputs = frozenset().union(*inputs) - reduced_vars
+
+    # optimize path with greedy opt_einsum optimizer
+    # TODO switch to new 'auto' strategy when it's released
+    path = greedy(inputs, outputs, size_dict)
+
+    # convert path IR back to sequence of Reduce(Finitary(...))
+
+    # first prepare a reduce_dim counter to avoid early reduction
+    reduce_dim_counter = collections.Counter()
+    for input in inputs:
+        reduce_dim_counter.update({d: 1 for d in input})
+
+    reduce_op, finitary_op = op, arg.op
+    operands = list(arg.operands)
+    for (a, b) in path:
+        ta = operands[a]
+        tb = operands.pop(b)
+
+        # don't reduce a dimension too early - keep a collections.Counter
+        # and only reduce when the dimension is removed from all lhs terms in path
+        reduce_dim_counter.subtract({d: 1 for d in reduced_vars & frozenset(ta.inputs.keys())})
+        reduce_dim_counter.subtract({d: 1 for d in reduced_vars & frozenset(tb.inputs.keys())})
+
+        # reduce variables that don't appear in other terms
+        both_vars = frozenset(ta.inputs.keys()) | frozenset(tb.inputs.keys())
+        path_end_reduced_vars = frozenset(d for d in reduced_vars & both_vars
+                                          if reduce_dim_counter[d] == 0)
+
+        # count new appearance of variables that aren't reduced
+        reduce_dim_counter.update({d: 1 for d in reduced_vars & (both_vars - path_end_reduced_vars)})
+
+        path_end = Binary(finitary_op, ta, tb)
+        if path_end_reduced_vars:
+            path_end = Reduce(reduce_op, path_end, path_end_reduced_vars)
+
+        operands[a] = path_end
+
+    # reduce any remaining dims, if necessary
+    final_reduced_vars = frozenset(d for (d, count) in reduce_dim_counter.items()
+                                   if count > 0) & reduced_vars
+    if final_reduced_vars:
+        path_end = Reduce(reduce_op, path_end, final_reduced_vars)
+    return path_end
+
+
+def apply_optimizer(x):
+
+    with interpretation(associate):
+        x = reinterpret(x)
+
+    with interpretation(distribute):
+        x = reinterpret(x)
+
+    with interpretation(optimize):
+        x = reinterpret(x)
+
+    return reinterpret(x)  # use previous interpretation
---
+++ test/test_optimizer.py
@@ -0,0 +1,103 @@
+from __future__ import absolute_import, division, print_function
+
+import pytest
+from collections import OrderedDict
+
+import opt_einsum
+import torch
+
+import funsor
+
+from funsor.distributions import Categorical
+from funsor.domains import bint
+from funsor.interpreter import interpretation, reinterpret
+from funsor.optimizer import apply_optimizer
+from funsor.terms import reflect, Variable
+from funsor.torch import Tensor
+
+from funsor.testing import make_einsum_example, naive_einsum
+
+
+def make_chain_einsum(num_steps):
+    inputs = [str(opt_einsum.get_symbol(0))]
+    for t in range(num_steps):
+        inputs.append(str(opt_einsum.get_symbol(t)) + str(opt_einsum.get_symbol(t+1)))
+    equation = ",".join(inputs) + "->"
+    return equation
+
+
+def make_hmm_einsum(num_steps):
+    inputs = [str(opt_einsum.get_symbol(0))]
+    for t in range(num_steps):
+        inputs.append(str(opt_einsum.get_symbol(t)) + str(opt_einsum.get_symbol(t+1)))
+        inputs.append(str(opt_einsum.get_symbol(t+1)))
+    equation = ",".join(inputs) + "->"
+    return equation
+
+
+OPTIMIZED_EINSUM_EXAMPLES = [
+    make_chain_einsum(t) for t in range(2, 50, 10)
+] + [
+    make_hmm_einsum(t) for t in range(2, 50, 10)
+]
+
+
+@pytest.mark.parametrize('equation', OPTIMIZED_EINSUM_EXAMPLES)
+@pytest.mark.parametrize('backend', ['pyro.ops.einsum.torch_log'])
+def test_optimized_einsum(equation, backend):
+    inputs, outputs, sizes, operands, funsor_operands = make_einsum_example(equation)
+    expected = opt_einsum.contract(equation, *operands, backend=backend)
+    with interpretation(reflect):
+        naive_ast = naive_einsum(equation, *funsor_operands, backend=backend)
+        optimized_ast = apply_optimizer(naive_ast)
+    actual = reinterpret(optimized_ast)  # eager by default
+
+    assert isinstance(actual, funsor.Tensor) and len(outputs) == 1
+    if len(outputs[0]) > 0:
+        actual = actual.align(tuple(outputs[0]))
+
+    assert expected.shape == actual.data.shape
+    assert torch.allclose(expected, actual.data)
+    for output in outputs:
+        for i, output_dim in enumerate(output):
+            assert output_dim in actual.inputs
+            assert actual.inputs[output_dim].dtype == sizes[output_dim]
+
+
+@pytest.mark.parametrize("eqn1,eqn2", [
+    ("ab,bc,cd->d", "de,ef,fg->"),
+])
+@pytest.mark.parametrize("optimize1", [False, True])
+@pytest.mark.parametrize("optimize2", [False, True])
+@pytest.mark.parametrize("backend1", ['torch'])
+@pytest.mark.parametrize("backend2", ['torch', 'pyro.ops.einsum.torch_log'])
+def test_nested_einsum(eqn1, eqn2, optimize1, optimize2, backend1, backend2):
+    inputs1, outputs1, sizes1, operands1, _ = make_einsum_example(eqn1, sizes=(3,))
+    inputs2, outputs2, sizes2, operands2, funsor_operands2 = make_einsum_example(eqn2, sizes=(3,))
+
+    # normalize the probs for ground-truth comparison
+    operands1 = [operand.abs() / operand.abs().sum(-1, keepdim=True)
+                 for operand in operands1]
+
+    expected1 = opt_einsum.contract(eqn1, *operands1, backend=backend1)
+    expected2 = opt_einsum.contract(outputs1[0] + "," + eqn2, *([expected1] + operands2), backend=backend2)
+
+    with interpretation(reflect):
+        funsor_operands1 = [
+            Categorical(probs=Tensor(
+                operand,
+                inputs=OrderedDict([(d, bint(sizes1[d])) for d in inp[:-1]])
+            ))(value=Variable(inp[-1], bint(sizes1[inp[-1]]))).exp()
+            for inp, operand in zip(inputs1, operands1)
+        ]
+
+        output1 = naive_einsum(eqn1, *funsor_operands1, backend=backend1)
+        output1 = apply_optimizer(output1) if optimize1 else output1
+        output2 = naive_einsum(outputs1[0] + "," + eqn2, *([output1] + funsor_operands2), backend=backend2)
+        output2 = apply_optimizer(output2) if optimize2 else output2
+
+    actual1 = reinterpret(output1)
+    actual2 = reinterpret(output2)
+
+    assert torch.allclose(expected1, actual1.data)
+    assert torch.allclose(expected2, actual2.data)
