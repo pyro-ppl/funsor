fb1cce5c8360624268128e0c2e46130762bb9e08
Fritz Obermeyer <fritzo@uber.com>
2019-03-08 12:28:40 -0800

    Sketch funsor.contract module

--- funsor/einsum.py
+++ funsor/einsum.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, division, print_function
 
-from collections import defaultdict, OrderedDict
 from six.moves import reduce
 
 import funsor.ops as ops
 from funsor.interpreter import interpretation, reinterpret
 from funsor.optimizer import apply_optimizer
 from funsor.terms import Funsor, reflect
+from funsor.contract import sum_product
 
 
 def naive_einsum(eqn, *terms, **kwargs):
@@ -26,37 +26,6 @@ def naive_einsum(eqn, *terms, **kwargs):
     output_dims = frozenset(output)
     reduce_dims = input_dims - output_dims
     return reduce(prod_op, terms).reduce(sum_op, reduce_dims)
-
-
-def _partition(terms, sum_vars):
-    # Construct a bipartite graph between terms and the vars
-    neighbors = OrderedDict([(t, []) for t in terms])
-    for term in terms:
-        for dim in term.inputs.keys():
-            if dim in sum_vars:
-                neighbors[term].append(dim)
-                neighbors.setdefault(dim, []).append(term)
-
-    # Partition the bipartite graph into connected components for contraction.
-    components = []
-    while neighbors:
-        v, pending = neighbors.popitem()
-        component = OrderedDict([(v, None)])  # used as an OrderedSet
-        for v in pending:
-            component[v] = None
-        while pending:
-            v = pending.pop()
-            for v in neighbors.pop(v):
-                if v not in component:
-                    component[v] = None
-                    pending.append(v)
-
-        # Split this connected component into tensors and dims.
-        component_terms = tuple(v for v in component if isinstance(v, Funsor))
-        if component_terms:
-            component_dims = frozenset(v for v in component if not isinstance(v, Funsor))
-            components.append((component_terms, component_dims))
-    return components
 
 
 def naive_plated_einsum(eqn, *terms, **kwargs):
@@ -82,48 +51,19 @@ def naive_plated_einsum(eqn, *terms, **kwargs):
     assert isinstance(eqn, str)
     assert all(isinstance(term, Funsor) for term in terms)
     inputs, output = eqn.split('->')
+    inputs = inputs.split(',')
+    assert len(inputs) == len(terms)
     assert len(output.split(',')) == 1
-    input_dims = frozenset(d for inp in inputs.split(',') for d in inp)
+    input_dims = frozenset(d for inp in inputs for d in inp)
     output_dims = frozenset(d for d in output)
     plate_dims = frozenset(plates) - output_dims
     reduce_vars = input_dims - output_dims - frozenset(plates)
 
-    if output_dims:
+    output_plates = output_dims & plate_dims
+    if not all(output_plates.issubset(inp) for inp in inputs):
         raise NotImplementedError("TODO")
 
-    var_tree = {}
-    term_tree = defaultdict(list)
-    for term in terms:
-        ordinal = frozenset(term.inputs) & plate_dims
-        term_tree[ordinal].append(term)
-        for var in term.inputs:
-            if var not in plate_dims:
-                var_tree[var] = var_tree.get(var, ordinal) & ordinal
-
-    ordinal_to_var = defaultdict(set)
-    for var, ordinal in var_tree.items():
-        ordinal_to_var[ordinal].add(var)
-
-    # Direct translation of Algorithm 1
-    scalars = []
-    while term_tree:
-        leaf = max(term_tree, key=len)
-        leaf_terms = term_tree.pop(leaf)
-        leaf_reduce_vars = ordinal_to_var[leaf]
-        for (group_terms, group_vars) in _partition(leaf_terms, leaf_reduce_vars):
-            term = reduce(prod_op, group_terms).reduce(sum_op, group_vars)
-            remaining_vars = frozenset(term.inputs) & reduce_vars
-            if not remaining_vars:
-                scalars.append(term.reduce(prod_op, leaf))
-            else:
-                new_plates = frozenset().union(
-                    *(var_tree[v] for v in remaining_vars))
-                if new_plates == leaf:
-                    raise ValueError("intractable!")
-                term = term.reduce(prod_op, leaf - new_plates)
-                term_tree[new_plates].append(term)
-
-    return reduce(prod_op, scalars)
+    return sum_product(sum_op, prod_op, terms, reduce_vars, plate_dims)
 
 
 def einsum(eqn, *terms, **kwargs):
