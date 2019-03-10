ef7358bef97827f123d5f0801515f610726966d8
Fritz Obermeyer <fritzo@uber.com>
2019-03-09 21:41:44 -0800

    Add tests for reduction

--- test/test_delta.py
+++ test/test_delta.py
@@ -2,9 +2,10 @@ from __future__ import absolute_import, division, print_function
 
 import torch
 
+import funsor.ops as ops
 from funsor.delta import Delta
 from funsor.domains import reals
-from funsor.terms import Variable
+from funsor.terms import Number, Variable
 from funsor.testing import check_funsor
 from funsor.torch import Tensor
 
@@ -17,7 +18,6 @@ def test_eager_subs_variable():
 
 
 def test_eager_subs_ground():
-    v = Variable('v', reals(3))
     point1 = Tensor(torch.randn(3))
     point2 = Tensor(torch.randn(3))
     d = Delta('foo', point1)
@@ -33,3 +33,9 @@ def test_add_delta_funsor():
     expr = -(1 + x ** 2).log()
     assert d + expr is d + expr(x=y)
     assert expr + d is expr(x=y) + d
+
+
+def test_reduce():
+    point = Tensor(torch.randn(3))
+    d = Delta('foo', point)
+    assert d.reduce(ops.logaddexp, frozenset(['foo'])) is Number(0)
