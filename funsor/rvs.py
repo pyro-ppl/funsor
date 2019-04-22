1e4586373c80979c2c767de71529b3c15e8e618d
Eli <eli.bingham@uber.com>
2019-04-22 13:43:16 -0700

    isolate randomvariable changes

---
+++ funsor/rvs.py
@@ -0,0 +1,184 @@
+from __future__ import absolute_import, division, print_function
+
+import functools
+import pyro.distributions as dist
+from collections import OrderedDict
+from six import add_metaclass
+from six.moves import reduce
+
+import funsor.distributions
+import funsor.interpreter as interpreter
+import funsor.ops as ops
+from funsor.contract import Contract
+from funsor.distributions import Normal, numbers_to_tensors
+from funsor.domains import Domain, find_domain, reals
+from funsor.ops import AssociativeOp, TransformOp
+from funsor.optimizer import optimize
+from funsor.product import Product
+from funsor.terms import Binary, Funsor, FunsorMeta, Reduce, Subs, Unary, Variable, \
+    eager, to_funsor
+from funsor.torch import Tensor, align_tensors
+
+
+#########################################################
+# Random variables built on PyTorch distributions
+#########################################################
+
+PDIST_TO_FDIST = {
+    dist.Normal: funsor.distributions.Normal,
+    dist.Categorical: funsor.distributions.Categorical,
+    dist.Bernoulli: funsor.distributions.Bernoulli,
+    dist.Delta: funsor.distributions.Delta,
+}
+
+
+class RandomVariableMeta(FunsorMeta):
+    """
+    Wrapper to fill in default values and convert Numbers to Tensors.
+    """
+    def __call__(cls, *args, **kwargs):
+        # TODO move omega-handling logic and to_funsor-ing logic here?
+        kwargs.update(zip(cls._ast_fields, args))
+        args = cls._fill_defaults(**kwargs)  # TODO get rid of _fill_defaults?
+        args = numbers_to_tensors(*args)
+        return super(RandomVariableMeta, cls).__call__(*args)
+
+
+@add_metaclass(RandomVariableMeta)
+class RandomVariable(Funsor):
+    """
+    Random Variable class backed by a PyTorch distribution.
+    Transforms like a sample, but carries a deferred random number generator call.
+    """
+    dist_class = "defined by derived classes"
+
+    def __init__(self, *args):
+        params = tuple(zip(self._ast_fields, args))
+        assert any(k == 'omega' for k, v in params)
+        inputs = OrderedDict()
+        for name, value in params:
+            assert isinstance(name, str)
+            assert isinstance(value, Funsor)
+            inputs.update(value.inputs)
+        inputs = OrderedDict(inputs)
+        output = reals()
+        super(RandomVariable, self).__init__(inputs, output)
+        self.params = OrderedDict(params)
+
+    def __repr__(self):
+        return '{}({})'.format(type(self).__name__,
+                               ', '.join('{}={}'.format(*kv) for kv in self.params))
+
+    def eager_subs(self, subs):
+        assert isinstance(subs, tuple)
+        if not any(k in self.inputs for k, v in subs):
+            return self
+        if any(k == self.params["omega"].name and not isinstance(v, Variable) for k, v in subs):
+            raise ValueError("Cannot substitute ground term into omega")
+        params = OrderedDict((k, Subs(v, subs)) for k, v in self.params.items())
+        return type(self)(**params)
+
+    @property
+    def omega(self):
+        return self.params["omega"]
+
+    @classmethod
+    def eager_dist(cls, **params):
+        params.pop('omega', None)
+        inputs, tensors = align_tensors(*params.values())
+        params = dict(zip(params, tensors))
+        return cls.dist_class(**params), inputs
+
+    def eager_reduce(self, op, reduced_vars):
+        if op is ops.add and self.omega.name in reduced_vars:
+            # expected value...
+            # TODO allow propagation of free discrete vars?
+            reduced_vars = reduced_vars - {self.omega.name}
+            new_params = {k: v.eager_reduce(op, reduced_vars) if reduced_vars else v
+                          for k, v in self.params.items() if k != 'omega'}
+            # XXX not always correct?
+            ground_dist, inputs = self.eager_dist(**new_params)
+            return Tensor(ground_dist.mean, inputs)
+        return super(RandomVariable, self).eager_reduce(op, reduced_vars)
+
+    def unscaled_sample(self, sampled_vars, sample_inputs):
+        if self.omega.name in sampled_vars:
+            sampled_vars = sampled_vars - {self.omega.name}
+            # FIXME this is not correct, might not share sample values
+            # just want to sample parameters here...
+            new_params = {k: v.sample(sampled_vars, sample_inputs) if sampled_vars else v
+                          for k, v in self.params.items() if k != 'omega'}
+            # TODO push a sample_shape built from sample_inputs...
+            ground_dist, inputs = self.eager_dist(**new_params)
+            return Tensor(ground_dist.sample(), inputs)
+        return super(RandomVariable, self).unscaled_sample(sampled_vars, sample_inputs)
+
+    def log_prob(self, value):
+        return PDIST_TO_FDIST[self.dist_class](
+            **{k: v for k, v in self.params.items() if k != 'omega' and k != 'value'}
+        )(value=value)
+
+
+class CategoricalRV(RandomVariable):
+    dist_class = dist.Categorical
+
+    @staticmethod
+    def _fill_defaults(probs, omega='omega'):
+        probs = to_funsor(probs)
+        omega = to_funsor(omega, reals())
+        return probs, omega
+
+    def __init__(self, probs, omega='omega'):
+        return super(CategoricalRV, self).__init__(probs, omega)
+
+
+class BernoulliRV(RandomVariable):
+    dist_class = dist.Bernoulli
+
+    @staticmethod
+    def _fill_defaults(probs, omega='omega'):
+        probs = to_funsor(probs)
+        omega = to_funsor(omega, reals())
+        return probs, omega
+
+    def __init__(self, probs, omega='omega'):
+        return super(BernoulliRV, self).__init__(probs, omega)
+
+
+class NormalRV(RandomVariable):
+    dist_class = dist.Normal
+
+    @staticmethod
+    def _fill_defaults(loc, scale, omega='omega'):
+        loc = to_funsor(loc)
+        scale = to_funsor(scale)
+        assert loc.output == reals()
+        assert scale.output == reals()
+        omega = to_funsor(omega, reals())
+        return loc, scale, omega
+
+    def __init__(self, loc, scale, omega='omega'):
+        return super(NormalRV, self).__init__(loc, scale, omega)
+
+
+class DeltaRV(RandomVariable):
+    dist_class = dist.Delta
+
+    @staticmethod
+    def _fill_defaults(v, log_density=0, omega='omega'):
+        v = to_funsor(v)
+        log_density = to_funsor(log_density)
+        omega = to_funsor(omega, reals())
+        return v, log_density, omega
+
+    def __init__(self, v, log_density, omega='omega'):
+        return super(DeltaRV, self).__init__(v, log_density, omega)
+
+
+#####################################################################
+# Handling invertible nonlinear unary transforms of RandomVariables
+#####################################################################
+
+@eager.register(Unary, TransformOp, RandomVariable)
+def eager_unary_rv(op, arg):
+    raise NotImplementedError("TODO implement invertible transforms of RandomVariables")
---
+++ test/test_random_variable.py
@@ -0,0 +1,103 @@
+from __future__ import absolute_import, division, print_function
+
+import pytest
+import torch
+from collections import OrderedDict
+
+import funsor.distributions as dist
+import funsor.ops as ops
+from funsor.domains import reals
+from funsor.rvs import RandomVariable, BernoulliRV, NormalRV, CategoricalRV, DeltaRV
+from funsor.terms import Funsor, Number, Variable
+from funsor.testing import assert_close
+from funsor.torch import Tensor
+
+
+SMOKE_TEST_CASES = [
+    ("NormalRV(0, 1)", NormalRV),
+    ("CategoricalRV(Tensor(torch.ones(3) / 3, OrderedDict([])))", CategoricalRV),
+    ("DeltaRV(5., 0)", DeltaRV),
+    ("BernoulliRV(0.5)", BernoulliRV),
+]
+
+
+@pytest.mark.parametrize("expr,expected_type", SMOKE_TEST_CASES)
+def test_sample_rv_smoke(expr, expected_type):
+    x = eval(expr)
+    assert isinstance(x, expected_type)
+    assert isinstance(x.sample(frozenset(['omega'])), Tensor)
+
+
+@pytest.mark.parametrize("expr,expected_type", SMOKE_TEST_CASES)
+def test_mean_rv_smoke(expr, expected_type):
+    x = eval(expr)
+    assert isinstance(x, expected_type)
+    assert isinstance(x.reduce(ops.add, frozenset(['omega'])), Tensor)
+
+
+@pytest.mark.parametrize("expr,expected_type", SMOKE_TEST_CASES)
+def test_subs_omega_ok_rv_smoke(expr, expected_type):
+    x = eval(expr)
+    assert isinstance(x, expected_type)
+    # it's ok to substitute another variable of the same type into omega
+    y = x(omega=Variable('omega2', reals()))
+    assert isinstance(y, expected_type)
+
+
+@pytest.mark.parametrize("expr,expected_type", SMOKE_TEST_CASES)
+def test_subs_omega_fail_rv_smoke(expr, expected_type):
+    x = eval(expr)
+    assert isinstance(x, expected_type)
+    # it's not ok to substitute an arbitrary expression into omega, no matter the type
+    with pytest.raises(ValueError):
+        z = x(omega=Number(1.5))
+        assert isinstance(z, Funsor)
+
+
+@pytest.mark.xfail(reason="affine transform of NormalRV not implemented")
+def test_transform_normalrv_simple():
+    x = NormalRV(loc=0., scale=1.)
+    y_actual = 2 * x + 1.
+
+    y_expected = NormalRV(loc=1., scale=2.)
+
+    actual = y_actual.reduce(ops.add)
+    expected = y_expected.reduce(ops.add)
+
+    assert_close(actual, expected)
+
+
+@pytest.mark.xfail(reason="integrate with RVs not implemented")
+def test_reduce_normalrv_simple():
+    # check that reducing substitution is the same as reducing joint densities
+    x = NormalRV(loc=0., scale=1., omega="omega_x")
+    y = dist.Normal(loc=x, scale=1., value=0.5)
+    actual = y.reduce(ops.add)
+
+    expected = (dist.Normal(loc=0., scale=1., value="x") + dist.Normal(loc="x", scale=1., value=0.5)).reduce(ops.logaddexp).exp()
+
+    assert_close(actual, expected)
+
+
+@pytest.mark.xfail(reason="add not implemented")
+def test_add_normalrv_simple():
+    x = NormalRV(loc=0., scale=1.)(omega='omegax')
+    y = NormalRV(loc=1., scale=0.5)(omega='omegay')
+    actual = x + y
+
+    expected = NormalRV(loc=0. + 1., scale=1. * 0.5)  # XXX is this right?
+
+    actual_mean = actual.reduce(ops.add)
+    expected_mean = expected.reduce(ops.add)
+
+    assert_close(actual_mean, expected_mean)
+
+
+@pytest.mark.xfail(reason="add not implemented")
+def test_add_normalrv_name_collision():
+    x = NormalRV(loc=0., scale=1.)(omega='omega2')
+    y = NormalRV(loc=1., scale=0.5)(omega='omega2')
+
+    # this should fail because the omegas are the same and we don't like that
+    with pytest.raises(NotImplementedError):
+        x + y
