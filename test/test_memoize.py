c02e54bffbffbcd3f44304fa9ccf9c8f108ae24b
Eli <eli.bingham@uber.com>
2019-07-14 00:00:22 -0700

    add a more targeted test

--- test/test_memoize.py
+++ test/test_memoize.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 import pytest
 import torch
 
+import funsor.ops as ops
 from funsor.distributions import Normal
 from funsor.einsum import einsum, naive_plated_einsum
 from funsor.interpreter import interpretation, reinterpret
@@ -33,7 +34,7 @@ EINSUM_EXAMPLES = [
 @pytest.mark.parametrize('equation,plates', EINSUM_EXAMPLES)
 @pytest.mark.parametrize('backend', ['torch', 'pyro.ops.einsum.torch_log'])
 @pytest.mark.parametrize('einsum_impl', [einsum, naive_plated_einsum])
-@pytest.mark.parametrize('same_lazy', [True, xfail_param(False, reason="issue w/ alpha conversion?")])
+@pytest.mark.parametrize('same_lazy', [True, xfail_param(False, reason="nested interpreters?")])
 def test_einsum_complete_sharing(equation, plates, backend, einsum_impl, same_lazy):
     inputs, outputs, sizes, operands, funsor_operands = make_einsum_example(equation)
 
@@ -54,7 +55,7 @@ def test_einsum_complete_sharing(equation, plates, backend, einsum_impl, same_la
 @pytest.mark.parametrize('equation,plates', EINSUM_EXAMPLES)
 @pytest.mark.parametrize('backend', ['torch', 'pyro.ops.einsum.torch_log'])
 @pytest.mark.parametrize('einsum_impl', [einsum, naive_plated_einsum])
-@pytest.mark.parametrize('same_lazy', [True, xfail_param(False, reason="issue w/ alpha conversion?")])
+@pytest.mark.parametrize('same_lazy', [True, xfail_param(False, reason="nested interpreters?")])
 def test_einsum_complete_sharing_reuse_cache(equation, plates, backend, einsum_impl, same_lazy):
     inputs, outputs, sizes, operands, funsor_operands = make_einsum_example(equation)
 
@@ -95,3 +96,43 @@ def test_memoize_sample(check_sample):
     if check_sample:
         assert x1 is x12
         assert x1 is x2
+
+
+@pytest.mark.parametrize("eqn1,eqn2", [("ab,bc,cd->d", "de,ef,fg->")])
+@pytest.mark.parametrize("einsum_impl1", [naive_plated_einsum, xfail_param(einsum, reason="nested interpreters?")])
+@pytest.mark.parametrize("einsum_impl2", [naive_plated_einsum, xfail_param(einsum, reason="nested interpreters?")])
+@pytest.mark.parametrize('backend1', ['torch', 'pyro.ops.einsum.torch_log'])
+@pytest.mark.parametrize('backend2', ['torch', 'pyro.ops.einsum.torch_log'])
+def test_nested_einsum_complete_sharing(eqn1, eqn2, einsum_impl1, einsum_impl2, backend1, backend2):
+
+    inputs1, outputs1, sizes1, operands1, funsor_operands1 = make_einsum_example(eqn1, sizes=(3,))
+    inputs2, outputs2, sizes2, operands2, funsor_operands2 = make_einsum_example(eqn2, sizes=(3,))
+
+    with memoize():
+        output1_1 = einsum_impl1(eqn1, *funsor_operands1, backend=backend1)
+        output2_1 = einsum_impl2(outputs1[0] + "," + eqn2, *([output1_1] + funsor_operands2), backend=backend2)
+
+        output1_2 = einsum_impl1(eqn1, *funsor_operands1, backend=backend1)
+        output2_2 = einsum_impl2(outputs1[0] + "," + eqn2, *([output1_2] + funsor_operands2), backend=backend2)
+
+    assert output1_1 is output1_2
+    assert output2_1 is output2_2
+
+
+def test_nested_complete_sharing_direct():
+
+    inputs, outputs, sizes, operands, funsor_operands = make_einsum_example("ab,bc,cd->d")
+    ab, bc, cd = funsor_operands
+
+    # avoids the complicated internal interpreter usage of the nested optimized einsum tests above
+    with interpretation(reflect):
+        c1 = (ab * bc).reduce(ops.add, frozenset({"a", "b"}))
+        d1 = (c1 * cd).reduce(ops.add, frozenset({"c"}))
+
+        # this does not trigger a second alpha-renaming
+        c2 = (ab * bc).reduce(ops.add, frozenset({"a", "b"}))
+        d2 = (c2 * cd).reduce(ops.add, frozenset({"c"}))
+
+    with memoize():
+        assert reinterpret(c1) is reinterpret(c2)
+        assert reinterpret(d1) is reinterpret(d2)
