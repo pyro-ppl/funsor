249da3c6722444a27235364c1f129a43da998e11
Fritz Obermeyer <fritzo@uber.com>
2019-07-23 23:47:11 -0700

    Sketch markov sum product algorithm

--- funsor/sum_product.py
+++ funsor/sum_product.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import re
 from collections import OrderedDict, defaultdict
 
 import torch
@@ -174,16 +175,89 @@ def sequential_sum_product(sum_op, prod_op, trans, time, prev, curr):
     assert isinstance(prev, str)
     assert isinstance(curr, str)
 
-    while trans.inputs["time"].size > 1:
-        duration = trans.inputs["time"].size
+    while trans.inputs[time].size > 1:
+        duration = trans.inputs[time].size
         even_duration = duration // 2 * 2
         # TODO support syntax
         # x = trans(time=slice(0, even_duration, 2), ...)
-        x = trans(time=Slice("time", 0, even_duration, 2, duration), curr="_drop")
-        y = trans(time=Slice("time", 1, even_duration, 2, duration), prev="_drop")
+        x = trans(**{time: Slice(time, 0, even_duration, 2, duration), curr: "_drop"})
+        y = trans(**{time: Slice(time, 1, even_duration, 2, duration), prev: "_drop"})
         contracted = prod_op(x, y).reduce(sum_op, "_drop")
         if duration > even_duration:
-            extra = trans(time=Slice("time", duration - 1, duration))
-            contracted = Cat((contracted, extra), "time")
+            extra = trans(**{time: Slice(time, duration - 1, duration)})
+            contracted = Cat((contracted, extra), time)
         trans = contracted
-    return trans(time=0)
+    return trans(**{time: 0})
+
+
+def parse_lags(name):
+    lags = defaultdict(0)
+    while True:
+        match = re.match(r"(.*)\(([^=]+)=([0-9]+)\)", name)
+        if match:
+            name, time, lag = match.groups()
+            lags[time] = int(lag)
+    return name, lags
+
+
+def format_lags(name, lags):
+    parts = ["name"]
+    for time, lag in sorted(lags.items()):
+        if lag:
+            parts.extend("({}={:d})".format(time, lag))
+    return "".join(parts)
+
+
+def increment_lags(dependent_vars, time):
+    step = {}
+    for name in dependent_vars:
+        name, lags = parse_lags(name)
+        lags[time] += 1
+        step[name] = format_lags(name, lags)
+    # TODO fix sum_vars
+    sum_vars = frozenset(step).intersection(step.values())
+    x_subs = {curr: prev for prev, curr in step.values()
+              if "TODO"}
+    y_subs = {prev: curr for prev, curr in step.values()
+              if "TODO"}
+    return x_subs, y_subs, sum_vars
+
+
+def markov_sum_product(sum_op, prod_op, arg, dependent_vars, prod_vars):
+    """
+    This encodes time dependency as a special convention in ``arg.inputs``; see
+    :func:`print_lags` and :func:`format_lags` for details.
+    """
+    assert dependent_vars.issubset(arg.inputs)
+    assert prod_vars.issubset(arg.inputs)
+    assert dependent_vars.isdisjoint(prod_vars)
+    lags = defaultdict(dict)
+    for name in dependent_vars:
+        for basename, name_lags in parse_lags(name):
+            for prod_var in prod_vars:
+                lags[prod_var][basename] = name_lags[prod_var]
+
+    # Eliminate time dimensions one at a time.
+    # TODO decide optimal order.
+    for time in prod_vars:
+        window = max(lags[time].values()) - min(lags[time].values())
+        if window == 0:
+            arg = arg.reduce(prod_op, time)
+            continue
+
+        # TODO does dependent_vars depend on time?
+        x_subs, y_subs, sum_vars = increment_lags(dependent_vars, time)
+        while arg.inputs[time].size > 1:
+            duration = arg.inputs[time].size - window + 1  # XXX Correct?
+            even_duration = duration // (1 + window) * (1 + window)
+            x_subs[time] = Slice(time, 0, even_duration, 1 + window, duration)
+            y_subs[time] = Slice(time, 1, even_duration, 1 + window, duration)
+            x = arg(**x_subs)
+            y = arg(**y_subs)
+            contracted = prod_op(x, y).reduce(sum_op, sum_vars)
+            if duration > even_duration:
+                extra = arg(**{time: Slice(time, even_duration, duration)})
+                contracted = Cat((contracted, extra), time)
+            arg = contracted
+        arg = arg(**{time: 0})
+    return arg
