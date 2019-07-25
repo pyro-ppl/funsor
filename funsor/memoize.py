9773f14ba4cab819b00581f032d09ab1cb27e434
eb8680 <eb8680@users.noreply.github.com>
2019-07-24 18:32:41 -0700

    Add a memoize interpreter/context manager (#160)
    * Add a memoize interpreter/context manager for common subexpression elimination
    
    * add test reusing cache
    
    * add a failing sample test
    
    * isolate failure to Joint.sample
    
    * add a more targeted test
    
    * refine set of xfailing cases
    
    * remove __future__
    
    * remove try/finally

---
+++ funsor/memoize.py
@@ -0,0 +1,23 @@
+from collections import Hashable
+from contextlib2 import contextmanager
+
+import funsor.interpreter as interpreter
+
+
+@contextmanager
+def memoize(cache=None):
+    """
+    Exploit cons-hashing to do implicit common subexpression elimination
+    """
+    if cache is None:
+        cache = {}
+
+    @interpreter.interpretation(interpreter._INTERPRETATION)  # use base
+    def memoize_interpretation(cls, *args):
+        key = (cls,) + tuple(id(arg) if not isinstance(arg, Hashable) else arg for arg in args)
+        if key not in cache:
+            cache[key] = cls(*args)
+        return cache[key]
+
+    with interpreter.interpretation(memoize_interpretation):
+        yield cache
