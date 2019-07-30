008e395bfee66b7c19bfcb7b45693dbd9d9815f9
Fritz Obermeyer <fritzo@uber.com>
2019-07-30 10:21:32 -0700

    Sketch Switching Linear Dynamical System as an MRF

--- funsor/pyro/__init__.py
+++ funsor/pyro/__init__.py
@@ -1,7 +1,9 @@
 from funsor.pyro.distribution import FunsorDistribution
-from funsor.pyro.hmm import DiscreteHMM
+from funsor.pyro.hmm import DiscreteHMM, GaussianMRF, SwitchingLinearMRF
 
 __all__ = [
     "DiscreteHMM",
     "FunsorDistribution",
+    "GaussianMRF",
+    "SwitchingLinearMRF",
 ]
--- funsor/pyro/hmm.py
+++ funsor/pyro/hmm.py
@@ -8,7 +8,7 @@ from funsor.interpreter import interpretation
 from funsor.pyro.convert import dist_to_funsor, funsor_to_tensor, tensor_to_funsor
 from funsor.pyro.distribution import FunsorDistribution
 from funsor.sum_product import sequential_sum_product
-from funsor.terms import Variable, lazy, Stack
+from funsor.terms import Stack, Variable, lazy, moment_matching
 
 
 class DiscreteHMM(FunsorDistribution):
@@ -154,4 +154,107 @@ class GaussianMRF(FunsorDistribution):
         new._trans = self._trans
         new._obs = self._obs
         super(GaussianMRF, new).__init__(self.funsor_dist, batch_shape, self.event_shape)
+        return new
+
+
+class SwitchingLinearMRF(FunsorDistribution):
+    """
+    Switching Linear Dynamical System represented as a Markov Random Field.
+
+    This uses the :func:`~funsor.terms.moment_matching` approximation.
+
+    Let ``class`` be the latent class, ``state`` be the latent multivariate
+    normal state, and ``value`` be the observed multivariate normal value.
+
+    :param ~torch.distributions.Categorical initial_cat: Represents
+        ``p(class[0])``.
+    :param ~torch.distributions.MultivariateNormal initial_mvn: Represents
+        ``p(state[0] | class[0])``.
+    :param ~torch.distributions.Categorical transition_cat: Represents
+        ``p(class[t+1] | class[t])``.
+    :param ~torch.distributions.MultivariateNormal transition_mvn: Represents
+        ``p(state[t], state[t+1] | class[t])``.
+    :param ~torch.distributions.MultivariateNormal observation_mvn: Represents
+        ``p(value[t+1], state[t+1] | class[t+1])``.
+    """
+    has_rsample = True
+
+    def __init__(self, initial_cat, initial_mvn, transition_cat, transition_mvn, observation_mvn):
+        assert isinstance(initial_cat, torch.distributions.Cateogorical)
+        assert isinstance(initial_mvn, torch.distributions.MultivariateNormal)
+        assert isinstance(transition_cat, torch.distributions.Categorical)
+        assert isinstance(transition_mvn, torch.distributions.MultivariateNormal)
+        assert isinstance(observation_mvn, torch.distributions.MultivariateNormal)
+
+        hidden_cardinality = initial_cat.param_shape()[-1]
+        hidden_dim = initial_mvn.event_shape[0]
+        obs_dim = observation_mvn.event_shape[0] - hidden_dim
+        assert transition_cat.param_shape()[-1] == hidden_cardinality
+        assert transition_mvn.event_shape[0] == hidden_dim + hidden_dim
+        shape = broadcast_shape(initial_cat.batch_shape + (1, hidden_cardinality),
+                                initial_mvn.batch_shape + (1, hidden_cardinality),
+                                transition_cat.batch_shape,
+                                transition_mvn.batch_shape,
+                                observation_mvn.batch_shape)
+        batch_shape, time_shape = shape[:-2], shape[-2:-1]
+        event_shape = time_shape + (obs_dim,)
+
+        # Convert distributions to funsors.
+        init = (dist_to_funsor(initial_cat)(value="class") +
+                dist_to_funsor(initial_mvn, ("class",))(value="state"))
+        trans_inputs = tensor_to_funsor(transition_mvn.loc, ("time", "class"), 1).inputs.copy()
+        trans_inputs["state"] = reals(hidden_dim)
+        trans_inputs["state(time=1)"] = reals(hidden_dim)
+        trans = (dist_to_funsor(transition_cat, ("time", "class"))(value="class(time=1)") +
+                 Gaussian(transition_mvn.loc, transition_mvn.precision_matrix, trans_inputs))
+        obs_inputs = tensor_to_funsor(observation_mvn.loc, ("time", "class(time=1)"), 1).inputs.copy()
+        obs_inputs["state(time=1)"] = reals(hidden_dim)
+        obs_inputs["value"] = reals(obs_dim)
+        obs = Gaussian(observation_mvn.loc, observation_mvn.precision_matrix, obs_inputs)
+
+        # Construct the joint funsor.
+        with interpretation(lazy):
+            # TODO perform math here once sequential_sum_product has been
+            #   implemented as a first-class funsor.
+            funsor_dist = Variable("value", obs.inputs["value"])  # a bogus value
+            # Until funsor_dist is defined, we save factors for hand-computation in .log_prob().
+            self._init = init
+            self._trans = trans
+            self._obs = obs
+
+        super(SwitchingLinearMRF, self).__init__(funsor_dist, batch_shape, event_shape)
+
+    # TODO remove this once self.funsor_dist is defined.
+    def log_prob(self, value):
+        ndims = max(len(self.batch_shape), value.dim() - 1)
+        value = tensor_to_funsor(value, ("time",), 1)
+
+        # Compare with pyro.distributions.hmm.GaussianMRF.log_prob().
+        logp_oh = self._trans + self._obs(value=value)
+        logp_h = self._trans + self._obs.reduce(ops.logaddexp, "value")
+        logp = Stack((logp_oh, logp_h), "_stack")
+        with interpretation(moment_matching):
+            logp = sequential_sum_product(ops.logaddexp, ops.add, logp, "time",
+                                          ("class", "state"), ("class", "state(time=1)"))
+            logp += self._init
+            logp = logp.reduce(ops.logaddexp,
+                               frozenset({"class", "state", "class(time=1)", "state(time=1)"}))
+        logp_oh, logp_h = logp(_stack=0), logp(_stack=1)
+        result = logp_oh - logp_h
+
+        result = funsor_to_tensor(result, ndims=ndims)
+        return result
+
+    # TODO remove this once self.funsor_dist is defined.
+    def _sample_delta(self, sample_shape):
+        raise NotImplementedError("TODO")
+
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(DiscreteHMM, _instance)
+        batch_shape = torch.Size(batch_shape)
+        new._has_rsample = self._has_rsample
+        new._init = self._init
+        new._trans = self._trans
+        new._obs = self._obs
+        super(SwitchingLinearMRF, new).__init__(self.funsor_dist, batch_shape, self.event_shape)
         return new
