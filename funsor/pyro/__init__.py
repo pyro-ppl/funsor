ef12480a14912dd34514475976dc78ba1b893565
Fritz Obermeyer <fritzo@uber.com>
2019-07-31 17:46:51 -0700

    Add GaussianDiscreteMRF to funsor/pyro/__init__.py

--- funsor/pyro/__init__.py
+++ funsor/pyro/__init__.py
@@ -1,8 +1,9 @@
 from funsor.pyro.distribution import FunsorDistribution
-from funsor.pyro.hmm import DiscreteHMM, GaussianMRF
+from funsor.pyro.hmm import DiscreteHMM, GaussianDiscreteMRF, GaussianMRF
 
 __all__ = [
     "DiscreteHMM",
     "FunsorDistribution",
+    "GaussianDiscreteMRF",
     "GaussianMRF",
 ]
