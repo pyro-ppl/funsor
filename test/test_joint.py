Fix normalization bugs in moment_matching interpretation

test/test_joint.py
@@ -8,9 +8,11 @@ import funsor.ops as ops
 from funsor.delta import Delta
 from funsor.domains import bint, reals
 from funsor.gaussian import Gaussian
+from funsor.integrate import Integrate
 from funsor.interpreter import interpretation
 from funsor.joint import Joint
-from funsor.terms import Number, Reduce, eager, moment_matching
+from funsor.montecarlo import monte_carlo_interpretation
+from funsor.terms import Number, Reduce, Variable, eager, moment_matching
 from funsor.testing import assert_close, random_gaussian, random_tensor, xfail_if_not_implemented
 from funsor.torch import Tensor
 
@@ -225,9 +227,11 @@ def test_reduce_moment_matching_univariate():
     info_vec = precision.matmul(loc.unsqueeze(-1)).squeeze(-1)
     discrete = Tensor(torch.tensor([1 - p, p]).log() + t, int_inputs)
     gaussian = Gaussian(info_vec, precision, inputs)
+    gaussian -= gaussian.log_normalizer
     joint = discrete + gaussian
     with interpretation(moment_matching):
         actual = joint.reduce(ops.logaddexp, 'i')
+    assert_close(actual.reduce(ops.logaddexp), joint.reduce(ops.logaddexp))
 
     expected_loc = torch.tensor([(2 * p - 1) * s1])
     expected_variance = (4 * p * (1 - p) * s1 ** 2
@@ -236,6 +240,7 @@ def test_reduce_moment_matching_univariate():
     expected_precision = torch.tensor([[1 / expected_variance]])
     expected_info_vec = expected_precision.matmul(expected_loc.unsqueeze(-1)).squeeze(-1)
     expected_gaussian = Gaussian(expected_info_vec, expected_precision, real_inputs)
+    expected_gaussian -= expected_gaussian.log_normalizer
     expected_discrete = Tensor(torch.tensor(t))
     expected = expected_discrete + expected_gaussian
     assert_close(actual, expected, atol=1e-5, rtol=None)
@@ -255,14 +260,17 @@ def test_reduce_moment_matching_multivariate():
     precision = torch.zeros(4, 1, 1) + torch.eye(2, 2)
     discrete = Tensor(torch.zeros(4), int_inputs)
     gaussian = Gaussian(loc, precision, inputs)
+    gaussian -= gaussian.log_normalizer
     joint = discrete + gaussian
     with interpretation(moment_matching):
         actual = joint.reduce(ops.logaddexp, 'i')
+    assert_close(actual.reduce(ops.logaddexp), joint.reduce(ops.logaddexp))
 
     expected_loc = torch.zeros(2)
     expected_covariance = torch.tensor([[101., 0.], [0., 2.]])
     expected_precision = expected_covariance.inverse()
     expected_gaussian = Gaussian(expected_loc, expected_precision, real_inputs)
+    expected_gaussian -= expected_gaussian.log_normalizer
     expected_discrete = Tensor(torch.tensor(4.).log())
     expected = expected_discrete + expected_gaussian
     assert_close(actual, expected, atol=1e-5, rtol=None)
@@ -277,7 +285,30 @@ def test_reduce_moment_matching_shape(interp):
     gaussian = random_gaussian(OrderedDict(
         [('k', bint(4)), ('l', bint(3)), ('m', bint(2)), ('y', reals()), ('z', reals(2))]))
     reduced_vars = frozenset(['i', 'k', 'l'])
+    real_vars = frozenset(k for k, d in gaussian.inputs.items() if d.dtype == "real")
     joint = delta + discrete + gaussian
     with interpretation(interp):
         actual = joint.reduce(ops.logaddexp, reduced_vars)
     assert set(actual.inputs) == set(joint.inputs) - reduced_vars
+    assert_close(actual.reduce(ops.logaddexp, real_vars),
+                 joint.reduce(ops.logaddexp, real_vars | reduced_vars))
+
+
+def test_reduce_moment_matching_moments():
+    x = Variable('x', reals(2))
+    gaussian = random_gaussian(OrderedDict(
+        [('i', bint(2)), ('j', bint(3)), ('x', reals(2))]))
+    with interpretation(moment_matching):
+        approx = gaussian.reduce(ops.logaddexp, 'j')
+    with monte_carlo_interpretation(s=bint(100000)):
+        actual = Integrate(approx, Number(1.), frozenset(['x']))
+        expected = Integrate(gaussian, Number(1.), frozenset(['j', 'x']))
+        assert_close(actual, expected, atol=1e-3, rtol=1e-3)
+
+        actual = Integrate(approx, x, frozenset(['x']))
+        expected = Integrate(gaussian, x, frozenset(['j', 'x']))
+        assert_close(actual, expected, atol=1e-2, rtol=1e-2)
+
+        actual = Integrate(approx, x * x, frozenset(['x']))
+        expected = Integrate(gaussian, x * x, frozenset(['j', 'x']))
+        assert_close(actual, expected, atol=1e-2, rtol=1e-2)