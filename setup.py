825b5760aeefbc76799203ba3e1c669e08e7da58
Fritz Obermeyer <fritzo@uber.com>
2019-10-22 14:22:54 -0700

    Clean up setup.py for 0.1 release

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name='funsor',
-    version='0.0.0',
+    version='0.1.0',
     description='Functional analysis + tensors + symbolic algebra',
     packages=find_packages(include=['funsor', 'funsor.*']),
     url='https://github.com/pyro-ppl/funsor',
     author='Uber AI Labs',
     author_email='fritzo@uber.com',
     install_requires=[
-        'contextlib2',
         'multipledispatch',
         'numpy>=1.7',
         'opt_einsum>=2.3.2',
-        'pyro-ppl>=0.3',
+        'pyro-ppl>=0.5',
         'torch>=1.3.0',
     ],
     extras_require={
@@ -23,7 +22,7 @@ setup(
             'pytest>=4.1',
             'pytest-xdist==1.27.0',
             'torchvision==0.2.1',
-            'pyro-api@https://api.github.com/repos/pyro-ppl/pyro-api/tarball/master',
+            'pyro-api>=0.1',
         ],
         'dev': [
             'flake8',
@@ -36,7 +35,6 @@ setup(
             'torchvision==0.2.1',
         ],
     },
-    tests_require=['flake8', 'pandas', 'pytest>=4.1'],
     keywords='probabilistic machine learning bayesian statistics pytorch',
     classifiers=[
         'Intended Audience :: Developers',
