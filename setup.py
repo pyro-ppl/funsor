25763ac93e794e5a7115a6662032e71695f5a3bf
Fritz Obermeyer <fritzo@uber.com>
2019-10-23 13:37:13 -0700

    Add pytest as core dependency (#290)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = '\n'.join(line for line in long_description.split('\n')[4:])
 
 setup(
     name='funsor',
-    version='0.1.1',
+    version='0.1.2',
     description='A tensor-like library for functions and distributions',
     packages=find_packages(include=['funsor', 'funsor.*']),
     url='https://github.com/pyro-ppl/funsor',
@@ -29,27 +29,27 @@ setup(
     },
     author='Uber AI Labs',
     author_email='fritzo@uber.com',
+    python_requires=">=3.6",
     install_requires=[
         'multipledispatch',
         'numpy>=1.7',
         'opt_einsum>=2.3.2',
         'pyro-ppl>=0.5',
+        'pytest>=4.1',
         'torch>=1.3.0',
     ],
     extras_require={
         'test': [
             'flake8',
             'pandas',
-            'pytest>=4.1',
+            'pyro-api>=0.1',
             'pytest-xdist==1.27.0',
             'torchvision==0.2.1',
-            'pyro-api>=0.1',
         ],
         'dev': [
             'flake8',
             'isort',
             'pandas',
-            'pytest>=4.1',
             'pytest-xdist==1.27.0',
             'sphinx>=2.0',
             'sphinx_rtd_theme',
