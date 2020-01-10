88624a32b9a993b26bd36d6e0a8ac962ede40cfa
fehiepsi <fehiepsi@gmail.com>
2020-01-10 02:19:59 -0500

    add scipy to setup file

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
             'pyro-api>=0.1',
             'pytest-xdist==1.27.0',
             'pillow-simd',
+            'scipy',
             'torchvision',
         ],
         'dev': [
@@ -52,6 +53,7 @@ setup(
             'isort',
             'pandas',
             'pytest-xdist==1.27.0',
+            'scipy',
             'sphinx>=2.0',
             'sphinx_rtd_theme',
             'pillow-simd',
