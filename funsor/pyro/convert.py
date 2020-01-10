9c777b1fb535cc53f8c524fb86a87d12540dc0a2
fehiepsi <fehiepsi@gmail.com>
2020-01-10 02:35:13 -0500

    fix import error

--- funsor/pyro/convert.py
+++ funsor/pyro/convert.py
@@ -26,11 +26,11 @@ from funsor.cnf import Contraction
 from funsor.delta import Delta
 from funsor.distributions import BernoulliLogits, MultivariateNormal, Normal
 from funsor.domains import bint, reals
-from funsor.gaussian import Gaussian, align_tensors
+from funsor.gaussian import Gaussian
 from funsor.interpreter import gensym
 from funsor.ops import cholesky
 from funsor.terms import Funsor, Independent, Variable, eager
-from funsor.torch import Tensor
+from funsor.torch import Tensor, align_tensors
 
 # Conversion functions use fixed names for Pyro batch dims, but
 # accept an event_inputs tuple for custom event dim names.
