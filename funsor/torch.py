50ba14ae2629c49443884f78e25db6ff3456b460
fehiepsi <fehiepsi@gmail.com>
2020-02-10 21:53:02 -0500

    use 2D cat when build block matrix

--- funsor/torch.py
+++ funsor/torch.py
@@ -141,6 +141,9 @@ def _diagonal(x, dim1, dim2):
 
 @ops.cat.register(int, [torch.Tensor])
 def _cat(dim, *x):
+    print("cat", [a.shape for a in x])
+    if len(x) == 1:
+        return x[0]
     return torch.cat(x, dim=dim)
 
 
