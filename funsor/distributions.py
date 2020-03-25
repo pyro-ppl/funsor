860118be6e88abfe5aef8a7d3eefeffbb743ab1a
Eli <eli.bingham@uber.com>
2020-03-25 15:33:49 -0700

    rename __BernoulliLogits

--- funsor/distributions.py
+++ funsor/distributions.py
@@ -253,7 +253,7 @@ def maskeddist_to_funsor(pyro_dist, output=None, dim_to_name=None):
 
 @to_funsor.register(torch.distributions.Bernoulli)
 def bernoulli_to_funsor(pyro_dist, output=None, dim_to_name=None):
-    new_pyro_dist = __BernoulliLogits(logits=pyro_dist.logits)
+    new_pyro_dist = _PyroWrapper_BernoulliLogits(logits=pyro_dist.logits)
     return torchdistribution_to_funsor(new_pyro_dist, output, dim_to_name)
 
 
