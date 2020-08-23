a71625c07f68ff3c4a7285318318f0d85209679f
Eli <eli@elibingham.com>
2020-08-22 20:02:34 -0400

    lint

--- funsor/expectation_propagation.py
+++ funsor/expectation_propagation.py
@@ -9,7 +9,7 @@ from funsor.adjoint import AdjointTape
 from funsor.cnf import Contraction
 from funsor.interpreter import dispatched_interpretation, interpretation
 from funsor.ops import DISTRIBUTIVE_OPS, PRODUCT_INVERSES, UNITS, AssociativeOp
-from funsor.terms import Funsor, eager, lazy, moment_matching, normalize, reflect, to_funsor
+from funsor.terms import Funsor, eager, lazy, moment_matching, normalize, reflect, to_funsor  # noqa: F401
 
 
 @dispatched_interpretation
@@ -55,18 +55,20 @@ def ep_generic_contraction_no_adjoint(red_op, bin_op, reduced_vars, terms):
     terms = [term for term in terms if term.inputs]
     approxs = [to_funsor(UNITS[bin_op]) for term in terms]  # initialize approximations to 1
     for step in range(10):  # fixpoint iteration
-        with interpretation(normalize)
+        with interpretation(normalize):
             full_approx = reduce(bin_op, approxs)
         with interpretation(moment_matching):
             for i, (term, approx) in enumerate(zip(terms, list(approxs))):
                 w = PRODUCT_INVERSES[bin_op](term, approx)
                 step_reduced_vars = reduced_vars.intersection(full_approx.inputs)
-                approxs[i] = bin_op(full_approx, w).reduce(red_op, step_reduced_vars.union(w.inputs) - frozenset(term.inputs))
+                approxs[i] = bin_op(full_approx, w).reduce(
+                    red_op, step_reduced_vars.union(w.inputs) - frozenset(term.inputs))
                 approxs[i] = PRODUCT_INVERSES[bin_op](approxs[i], approxs[i].reduce(red_op))
                 full_approx = bin_op(full_approx, PRODUCT_INVERSES[bin_op](approxs[i], approx))
 
     ws = [PRODUCT_INVERSES[bin_op](term, approx) for term, approx in zip(terms, approxs)]
     approx = reduce(bin_op, approxs)
     step_reduced_vars = reduced_vars.intersection(approx.inputs)
-    result = reduce(bin_op, scalar_terms + [bin_op(approx, w).reduce(red_op, step_reduced_vars.union(w.inputs)) for w in ws])
+    result = reduce(bin_op, scalar_terms + [
+        bin_op(approx, w).reduce(red_op, step_reduced_vars.union(w.inputs)) for w in ws])
     return result
