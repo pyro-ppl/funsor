0b8ab86a6373b30cdfd2b67ea26d66f5cf48c2ad
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2020-09-28 07:39:12 -0400

    Simplify examples

--- funsor/minipyro.py
+++ funsor/minipyro.py
@@ -49,7 +49,7 @@ class Distribution(object):
 
     # Draw a sample.
     def __call__(self):
-        with funsor.interpreter.interpretation(funsor.terms.eager):
+        with funsor.interpretation(funsor.terms.eager):
             dist = self.funsor_dist(value='value')
             delta = dist.sample(frozenset(['value']), sample_inputs=self.sample_inputs)
         if isinstance(delta, funsor.cnf.Contraction):
@@ -521,7 +521,7 @@ class TraceEnum_ELBO(ELBO):
     # TODO allow mixing of sampling and exact integration
     def __call__(self, model, guide, *args, **kwargs):
         if self.options.get("optimize", None):
-            with funsor.interpreter.interpretation(funsor.optimizer.optimize):
+            with funsor.interpretation(funsor.optimizer.optimize):
                 elbo_expr = elbo(model, guide, *args, **kwargs)
             return funsor.reinterpret(elbo_expr)
         return elbo(model, guide, *args, **kwargs)
