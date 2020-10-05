c932811befc6783aef8b87d930cba8865cd1e856
Du Phan <fehiepsi@gmail.com>
2020-10-05 13:24:29 -0500

    change xfail to skipip

--- test/test_distribution.py
+++ test/test_distribution.py
@@ -29,6 +29,16 @@ if get_backend() != "numpy":
     backend_dist = dist.dist
 
 
+def _skip_for_numpyro_version(version="0.2.4"):
+    if get_backend() == "jax":
+        import numpyro
+
+        if numpyro.__version__ <= version:
+            return True
+
+    return False
+
+
 @pytest.mark.xfail(get_backend() == "jax", reason="flaky test")
 @pytest.mark.parametrize('batch_shape', [(), (5,), (2, 3)], ids=str)
 @pytest.mark.parametrize('eager', [False, True])
@@ -240,6 +250,9 @@ def test_dirichlet_density(batch_shape, event_shape):
 
 @pytest.mark.parametrize('batch_shape', [(), (5,), (2, 3)], ids=str)
 @pytest.mark.parametrize('event_shape', [(1,), (4,), (5,)], ids=str)
+# TODO change xfail to skipif when NumPyro > 0.4.0 is released
+@pytest.mark.xfail(_skip_for_numpyro_version("0.4.0"),
+                   reason="DirichletMultinomial is not available in NumPyro 0.4.0")
 def test_dirichlet_multinomial_density(batch_shape, event_shape):
     batch_dims = ('i', 'j', 'k')[:len(batch_shape)]
     inputs = OrderedDict((k, Bint[v]) for k, v in zip(batch_dims, batch_shape))
@@ -269,6 +282,8 @@ def test_dirichlet_multinomial_density(batch_shape, event_shape):
 
 @pytest.mark.parametrize('batch_shape', [(), (5,), (2, 3)], ids=str)
 @pytest.mark.parametrize('event_shape', [(2,), (4,), (5,)], ids=str)
+@pytest.mark.xfail(_skip_for_numpyro_version("0.4.0"),
+                   reason="DirichletMultinomial is not available in NumPyro 0.4.0")
 def test_dirichlet_multinomial_conjugate(batch_shape, event_shape):
     max_count = 10
     batch_dims = ('i', 'j', 'k')[:len(batch_shape)]
@@ -831,21 +846,11 @@ def test_mvn_sample(with_lazy, batch_shape, sample_inputs, event_shape):
     _check_sample(funsor_dist_class, params, sample_inputs, inputs, atol=7e-2, num_samples=200000, with_lazy=with_lazy)
 
 
-def _skip_for_numpyro_2_4():
-    if get_backend() == "jax":
-        import numpyro
-
-        if numpyro.__version__ == "0.2.4":
-            return True
-
-    return False
-
-
 @pytest.mark.parametrize('sample_inputs', [(), ('ii',), ('ii', 'jj'), ('ii', 'jj', 'kk')])
 @pytest.mark.parametrize('batch_shape', [(), (5,), (2, 3)], ids=str)
 @pytest.mark.parametrize('event_shape', [(1,), (4,), (5,)], ids=str)
 @pytest.mark.parametrize('reparametrized', [True, False])
-@pytest.mark.skipif(_skip_for_numpyro_2_4(),
+@pytest.mark.skipif(_skip_for_numpyro_version("0.2.4"),
                     reason="Dirichlet samples might take 0/1 values in NumPyro 0.2.4")
 def test_dirichlet_sample(batch_shape, sample_inputs, event_shape, reparametrized):
     batch_dims = ('i', 'j', 'k')[:len(batch_shape)]
@@ -888,7 +893,7 @@ def test_bernoulliprobs_sample(batch_shape, sample_inputs):
 @pytest.mark.parametrize('sample_inputs', [(), ('ii',), ('ii', 'jj'), ('ii', 'jj', 'kk')])
 @pytest.mark.parametrize('batch_shape', [(), (5,), (2, 3)], ids=str)
 @pytest.mark.parametrize('reparametrized', [True, False])
-@pytest.mark.skipif(_skip_for_numpyro_2_4(),
+@pytest.mark.skipif(_skip_for_numpyro_version("0.2.4"),
                     reason="Dirichlet samples might take 0/1 values in NumPyro 0.2.4")
 def test_beta_sample(with_lazy, batch_shape, sample_inputs, reparametrized):
     batch_dims = ('i', 'j', 'k')[:len(batch_shape)]
