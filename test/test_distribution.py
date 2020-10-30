4fbc801c2c182df3cc503ab402dfca021aa38c3f
Eli <eli@elibingham.com>
2020-10-29 22:54:29 -0400

    more cleanup of _check_sample

--- test/test_distribution.py
+++ test/test_distribution.py
@@ -685,90 +685,95 @@ def test_von_mises_probs_density(batch_shape, syntax):
     assert_close(actual, expected)
 
 
-def _get_stat_diff(funsor_dist_class, sample_inputs, inputs, num_samples, statistic, with_lazy, params):
-    params = [Tensor(p, inputs) for p in params]
-    if isinstance(with_lazy, bool):
-        with interpretation(lazy if with_lazy else eager):
-            funsor_dist = funsor_dist_class(*params)
-    else:
-        funsor_dist = funsor_dist_class(*params)
+def default_dim_to_name(inputs_shape, event_inputs=None):
+    DIM_TO_NAME = tuple(map("_pyro_dim_{}".format, range(-100, 0)))
+    NAME_TO_DIM = dict(zip(DIM_TO_NAME, range(-100, 0)))
 
+    dim_to_name_list = TESTS_DIM_TO_NAME + event_inputs if event_inputs else DIM_TO_NAME
+    dim_to_name = OrderedDict(zip(
+        range(-len(inputs_shape), 0),
+        dim_to_name_list[len(dim_to_name_list) - len(inputs_shape):]))
+    name_to_dim = OrderedDict((name, dim) for dim, name in dim_to_name.items())
+    return name_to_dim
+
+
+def _get_stat(raw_dist, sample_shape, statistic, with_lazy):
+    dim_to_name, name_to_dim = default_dim_to_name(sample_shape + raw_dist.batch_shape)
+    with interpretation(lazy if with_lazy else eager):
+        funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
+
+    sample_inputs = ...  # TODO compute sample_inputs from dim_to_name
     rng_key = None if get_backend() == "torch" else np.array([0, 0], dtype=np.uint32)
     sample_value = funsor_dist.sample(frozenset(['value']), sample_inputs, rng_key=rng_key)
-    expected_inputs = OrderedDict(
-        tuple(sample_inputs.items()) + tuple(inputs.items()) + (('value', funsor_dist.inputs['value']),)
-    )
+    expected_inputs = OrderedDict(tuple(sample_inputs.items()) + tuple(funsor_dist.inputs.items()))
     check_funsor(sample_value, expected_inputs, Real)
 
-    if sample_inputs:
-        if statistic == "mean":
-            actual_stat = Integrate(
-                sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
-            ).reduce(ops.add, frozenset(sample_inputs))
-            expected_stat = funsor_dist.mean()
-        elif statistic == "variance":
-            actual_mean = Integrate(
-                sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
-            ).reduce(ops.add, frozenset(sample_inputs))
-            actual_stat = Integrate(
-                sample_value,
-                (Variable('value', funsor_dist.inputs['value']) - actual_mean) ** 2,
-                frozenset(['value'])
-            ).reduce(ops.add, frozenset(sample_inputs))
-            expected_stat = funsor_dist.variance()
-        elif statistic == "entropy":
-            actual_stat = -Integrate(
-                sample_value, funsor_dist, frozenset(['value'])
-            ).reduce(ops.add, frozenset(sample_inputs))
-            expected_stat = funsor_dist.entropy()
-        else:
-            raise ValueError("invalid test statistic")
+    if statistic == "mean":
+        actual_stat = Integrate(
+            sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+        expected_stat = funsor_dist.mean()
+    elif statistic == "variance":
+        actual_mean = Integrate(
+            sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+        actual_stat = Integrate(
+            sample_value,
+            (Variable('value', funsor_dist.inputs['value']) - actual_mean) ** 2,
+            frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+        expected_stat = funsor_dist.variance()
+    elif statistic == "entropy":
+        actual_stat = -Integrate(
+            sample_value, funsor_dist, frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+        expected_stat = funsor_dist.entropy()
+    else:
+        raise ValueError("invalid test statistic")
 
-        diff = actual_stat.reduce(ops.add).data - expected_stat.reduce(ops.add).data
-        return diff.sum(), diff
+    return actual_stat.reduce(ops.add), expected_stat.reduce(ops.add)
 
 
-def _check_sample(raw_dist, sample_shape=(), atol=1e-2,
-                  num_samples=100000, statistic="mean", skip_grad=False, with_lazy=None):
-    """utility that compares a Monte Carlo estimate of a distribution mean with the true mean"""
-    samples_per_dim = int(num_samples ** (1./max(1, len(sample_inputs))))
-    sample_inputs = OrderedDict((k, Bint[samples_per_dim]) for k in sample_inputs)
-    _get_stat_diff_fn = functools.partial(
-        _get_stat_diff, funsor_dist_class, sample_inputs, inputs, num_samples, statistic, with_lazy)
+def _check_sample_grads(raw_dist, sample_shape=(), atol=1e-2, statistic="mean", with_lazy=False):
+
+    def _get_stat_diff_fn(raw_dist):
+        actual_stat, expected_stat = _get_stat(raw_dist, sample_shape, statistic, with_lazy)
+        return to_data((actual_stat - expected_stat).sum())
 
     if get_backend() == "torch":
         import torch
 
+        # TODO compute params here
         for param in params:
             param.requires_grad_()
 
-        res = _get_stat_diff_fn(params)
-        if sample_inputs:
-            diff_sum, diff = res
-            assert_close(diff, ops.new_zeros(diff, diff.shape), atol=atol, rtol=None)
-            if not skip_grad:
-                diff_grads = torch.autograd.grad(diff_sum, params, allow_unused=True)
-                for diff_grad in diff_grads:
-                    assert_close(diff_grad, ops.new_zeros(diff_grad, diff_grad.shape), atol=atol, rtol=None)
+        diff = _get_stat_diff_fn(raw_dist)
+        assert_close(diff, ops.new_zeros(diff, diff.shape), atol=atol, rtol=None)
+        diff_grads = torch.autograd.grad(diff, params, allow_unused=True)
+        for diff_grad in diff_grads:
+            assert_close(diff_grad, ops.new_zeros(diff_grad, diff_grad.shape), atol=atol, rtol=None)
+
     elif get_backend() == "jax":
         import jax
 
-        if sample_inputs:
-            if skip_grad:
-                _, diff = _get_stat_diff_fn(params)
-                assert_close(diff, ops.new_zeros(diff, diff.shape), atol=atol, rtol=None)
-            else:
-                (_, diff), diff_grads = jax.value_and_grad(_get_stat_diff_fn, has_aux=True)(params)
-                assert_close(diff, ops.new_zeros(diff, diff.shape), atol=atol, rtol=None)
-                for diff_grad in diff_grads:
-                    assert_close(diff_grad, ops.new_zeros(diff_grad, diff_grad.shape), atol=atol, rtol=None)
-        else:
-            _get_stat_diff_fn(params)
+        # TODO compute gradient wrt distribution instance PyTree
+        diff, diff_grads = jax.value_and_grad(lambda *args: _get_stat_diff_fn(*args).sum(), has_aux=True)(params)
+        assert_close(diff, ops.new_zeros(diff, diff.shape), atol=atol, rtol=None)
+        for diff_grad in diff_grads:
+            assert_close(diff_grad, ops.new_zeros(diff_grad, diff_grad.shape), atol=atol, rtol=None)
+
+
+def _check_sample(raw_dist, sample_shape=(), atol=1e-2, statistic="mean", with_lazy=False):
+
+    actual_stat, expected_stat = _get_stat(raw_dist, sample_shape, statistic, with_lazy)
+    check_funsor(actual_stat, expected_stat.inputs, expected_stat.output)
+    if sample_inputs:
+        assert_close(actual_stat, expected_stat, atol=atol, rtol=None)
 
 
 def _check_distribution_to_funsor(raw_dist, expected_value_domain):
 
-    # TODO specify dim_to_name
+    dim_to_name, name_to_dim = default_dim_to_name(raw_dist.batch_shape)
     funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
     actual_dist = to_data(funsor_dist, name_to_dim=name_to_dim)
     
@@ -784,21 +789,24 @@ def _check_distribution_to_funsor(raw_dist, expected_value_domain):
 
 def _check_log_prob(raw_dist, expected_value_domain):
 
-    # TODO specify dim_to_name
+    dim_to_name, name_to_dim = default_dim_to_name(raw_dist.batch_shape)
     funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
-    expected_inputs = {name: raw_dist.batch_shape[dim] for dim, name in dim_to_name.items()}
+    expected_inputs = {name: funsor.Bint[raw_dist.batch_shape[dim]] for dim, name in dim_to_name.items()}
     expected_inputs.update({"value": expected_value_domain})
 
     check_funsor(funsor_dist, expected_inputs, funsor.Real)
 
-    # TODO handle JAX rng here
-    expected_logprob = to_funsor(raw_dist.log_prob(raw_dist.sample()), output=funsor.Real, dim_to_name=dim_to_name)
+    if get_backend() == "jax":
+        raw_value = raw_dist.sample(rng_key=np.array([0, 0], dtype=np.uint32))
+    else:
+        raw_value = raw_dist.sample()
+    expected_logprob = to_funsor(raw_dist.log_prob(raw_value), output=funsor.Real, dim_to_name=dim_to_name)
     assert_close(funsor_dist(value=value), expected_logprob)
 
 
 def _check_enumerate_support(raw_dist, expand=False):
 
-    # TODO specify dim_to_name
+    dim_to_name, name_to_dim = default_dim_to_name(raw_dist.batch_shape)
     funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
 
     assert getattr(raw_dist, "has_enumerate_support", False) == funsor_dist.has_enumerate_support
