bace6583bcd2f9ec886866b5950e24027634a276
Eli <eli@elibingham.com>
2020-10-29 23:26:36 -0400

    nits

--- test/test_distribution_2.py
+++ test/test_distribution_2.py
@@ -26,63 +26,6 @@ if get_backend() != "numpy":
     backend_dist = dist.dist
 
 
-def _skip_for_numpyro_version(version="0.2.4"):
-    if get_backend() == "jax":
-        import numpyro
-
-        if numpyro.__version__ <= version:
-            return True
-
-    return False
-
-
-def default_dim_to_name(inputs_shape, event_inputs=None):
-    DIM_TO_NAME = tuple(map("_pyro_dim_{}".format, range(-100, 0)))
-    dim_to_name_list = DIM_TO_NAME + event_inputs if event_inputs else DIM_TO_NAME
-    dim_to_name = OrderedDict(zip(
-        range(-len(inputs_shape), 0),
-        dim_to_name_list[len(dim_to_name_list) - len(inputs_shape):]))
-    name_to_dim = OrderedDict((name, dim) for dim, name in dim_to_name.items())
-    return name_to_dim
-
-
-def _get_stat(raw_dist, sample_shape, statistic, with_lazy):
-    dim_to_name, name_to_dim = default_dim_to_name(sample_shape + raw_dist.batch_shape)
-    with interpretation(lazy if with_lazy else eager):
-        funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
-
-    sample_inputs = ...  # TODO compute sample_inputs from dim_to_name
-    rng_key = None if get_backend() == "torch" else np.array([0, 0], dtype=np.uint32)
-    sample_value = funsor_dist.sample(frozenset(['value']), sample_inputs, rng_key=rng_key)
-    expected_inputs = OrderedDict(tuple(sample_inputs.items()) + tuple(funsor_dist.inputs.items()))
-    check_funsor(sample_value, expected_inputs, funsor.Real)
-
-    if statistic == "mean":
-        actual_stat = Integrate(
-            sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
-        ).reduce(ops.add, frozenset(sample_inputs))
-        expected_stat = funsor_dist.mean()
-    elif statistic == "variance":
-        actual_mean = Integrate(
-            sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
-        ).reduce(ops.add, frozenset(sample_inputs))
-        actual_stat = Integrate(
-            sample_value,
-            (Variable('value', funsor_dist.inputs['value']) - actual_mean) ** 2,
-            frozenset(['value'])
-        ).reduce(ops.add, frozenset(sample_inputs))
-        expected_stat = funsor_dist.variance()
-    elif statistic == "entropy":
-        actual_stat = -Integrate(
-            sample_value, funsor_dist, frozenset(['value'])
-        ).reduce(ops.add, frozenset(sample_inputs))
-        expected_stat = funsor_dist.entropy()
-    else:
-        raise ValueError("invalid test statistic")
-
-    return actual_stat.reduce(ops.add), expected_stat.reduce(ops.add)
-
-
 ##################################################
 # Test cases
 ##################################################
@@ -100,12 +43,57 @@ TEST_CASES = [
 # Generic tests
 ###########################
 
+def _default_dim_to_name(inputs_shape, event_inputs=None):
+    DIM_TO_NAME = tuple(map("_pyro_dim_{}".format, range(-100, 0)))
+    dim_to_name_list = DIM_TO_NAME + event_inputs if event_inputs else DIM_TO_NAME
+    dim_to_name = OrderedDict(zip(
+        range(-len(inputs_shape), 0),
+        dim_to_name_list[len(dim_to_name_list) - len(inputs_shape):]))
+    name_to_dim = OrderedDict((name, dim) for dim, name in dim_to_name.items())
+    return name_to_dim
+
+
+def _get_stat(raw_dist, sample_shape, statistic, with_lazy):
+    dim_to_name, name_to_dim = _default_dim_to_name(sample_shape + raw_dist.batch_shape)
+    with interpretation(lazy if with_lazy else eager):
+        funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
+
+    sample_inputs = ...  # TODO compute sample_inputs from dim_to_name
+    rng_key = None if get_backend() == "torch" else np.array([0, 0], dtype=np.uint32)
+    sample_value = funsor_dist.sample(frozenset(['value']), sample_inputs, rng_key=rng_key)
+    expected_inputs = OrderedDict(tuple(sample_inputs.items()) + tuple(funsor_dist.inputs.items()))
+    check_funsor(sample_value, expected_inputs, funsor.Real)
+
+    expected_stat = getattr(funsor_dist, statistic)()
+    if statistic == "mean":
+        actual_stat = Integrate(
+            sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+    elif statistic == "variance":
+        actual_mean = Integrate(
+            sample_value, Variable('value', funsor_dist.inputs['value']), frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+        actual_stat = Integrate(
+            sample_value,
+            (Variable('value', funsor_dist.inputs['value']) - actual_mean) ** 2,
+            frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+    elif statistic == "entropy":
+        actual_stat = -Integrate(
+            sample_value, funsor_dist, frozenset(['value'])
+        ).reduce(ops.add, frozenset(sample_inputs))
+    else:
+        raise ValueError("invalid test statistic: {}".format(statistic))
+
+    return actual_stat.reduce(ops.add), expected_stat.reduce(ops.add)
+
+
 @pytest.mark.parametrize("case", TEST_CASES)
 def test_generic_distribution_to_funsor(case):
 
     raw_dist, expected_value_domain = case.raw_dist, case.expected_value_domain
 
-    dim_to_name, name_to_dim = default_dim_to_name(raw_dist.batch_shape)
+    dim_to_name, name_to_dim = _default_dim_to_name(raw_dist.batch_shape)
     funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
     actual_dist = to_data(funsor_dist, name_to_dim=name_to_dim)
 
@@ -124,7 +112,7 @@ def test_generic_log_prob(case):
 
     raw_dist, expected_value_domain = case.raw_dist, case.expected_value_domain
 
-    dim_to_name, name_to_dim = default_dim_to_name(raw_dist.batch_shape)
+    dim_to_name, name_to_dim = _default_dim_to_name(raw_dist.batch_shape)
     funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
     expected_inputs = {name: funsor.Bint[raw_dist.batch_shape[dim]] for dim, name in dim_to_name.items()}
     expected_inputs.update({"value": expected_value_domain})
@@ -146,7 +134,7 @@ def test_generic_enumerate_support(case, expand):
 
     raw_dist = case.raw_dist
 
-    dim_to_name, name_to_dim = default_dim_to_name(raw_dist.batch_shape)
+    dim_to_name, name_to_dim = _default_dim_to_name(raw_dist.batch_shape)
     funsor_dist = to_funsor(raw_dist, output=funsor.Real, dim_to_name=dim_to_name)
 
     assert getattr(raw_dist, "has_enumerate_support", False) == funsor_dist.has_enumerate_support
