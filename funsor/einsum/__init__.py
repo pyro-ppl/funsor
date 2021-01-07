13b537cf43cefdcb1fcf1b1174d91d7eae5041d3
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-01-07 17:11:33 -0500

    Fix einsum

--- funsor/einsum/__init__.py
+++ funsor/einsum/__init__.py
@@ -49,7 +49,8 @@ def naive_contract_einsum(eqn, *terms, **kwargs):
     assert len(output.split(',')) == 1
     input_dims = frozenset(d for inp in inputs for d in inp)
     output_dims = frozenset(d for d in output)
-    reduced_vars = input_dims - output_dims
+    all_inputs = {k: v for term in terms for k, v in term.inputs.items()}
+    reduced_vars = {k: all_inputs[k] for k in input_dims - output_dims}
     return Contraction(sum_op, prod_op, reduced_vars, *terms)
 
 
