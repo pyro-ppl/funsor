6bede2d7cbd30f9e38c5f30a453c5df5ac5592f5
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-01-25 19:49:47 -0500

    Implement a Funsor factory based on type hints

---
+++ funsor/factory.py
@@ -0,0 +1,101 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+import typing
+from collections import OrderedDict
+
+import makefun
+
+from funsor.interpreter import debug_logged
+from funsor.terms import Funsor, FunsorMeta, Variable, eager, to_funsor
+
+
+def _erase_types(fn):
+    def result(*args):
+        return fn(*args)
+    result.__name__ = fn.__name__
+    result.__module__ = fn.__module__
+    return debug_logged(result)
+
+
+class FreshMeta(type):
+    def __getitem__(cls, fn):
+        return Fresh(fn)
+
+
+class Fresh(metaclass=FreshMeta):
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __call__(self, *domains):
+        return self.fn(*domains)
+
+
+class Bound:
+    pass
+
+
+def make_funsor(fn):
+    input_types = typing.get_type_hints(fn)
+    output_type = input_types.pop("return")
+    hints = tuple(input_types.values())
+
+    class ResultMeta(FunsorMeta):
+        def __call__(cls, *args):
+            args = list(args)
+
+            # Compute domains of bound variables.
+            for i, (name, arg) in enumerate(zip(cls._ast_fields, args)):
+                hint = input_types[name]
+                if hint is Funsor:  # TODO support domains
+                    args[i] = to_funsor(arg)
+                elif hint is Bound:
+                    for other in args:
+                        if isinstance(other, Funsor):
+                            domain = other.inputs.get(arg, None)
+                            if domain is not None:
+                                arg = to_funsor(arg, domain)
+                    if not isinstance(arg, Variable):
+                        raise ValueError(f"Cannot infer domain of {name}={arg}")
+                    args[i] = arg
+                elif not isinstance(hint, Fresh):
+                    raise TypeError(f"Invalid type hint {name}: {hint}")
+
+            # Compute domains of fresh variables.
+            dependent_args = [arg.output for hint, arg in zip(hints, args)
+                              if hint in (Funsor, Bound)]
+            for i, (hint, arg) in enumerate(zip(hints, args)):
+                if isinstance(hint, Fresh):
+                    domain = hint(*dependent_args)
+                    args[i] = to_funsor(arg, domain)
+            return super().__call__(*args)
+
+    @makefun.with_signature("__init__({})".format(", ".join(["self"] + list(input_types))))
+    def __init__(self, **kwargs):
+        args = tuple(kwargs[k] for k in self._ast_fields)
+        dependent_args = [arg.output for hint, arg in zip(hints, args)
+                          if hint in (Funsor, Bound)]
+        output = output_type(*dependent_args)
+        inputs = OrderedDict()
+        fresh = set()
+        bound = {}
+        for hint, arg in zip(hints, args):
+            if hint is Funsor:
+                inputs.update(arg.inputs)
+        for hint, arg in zip(hints, args):
+            if hint is Bound:
+                bound[arg.name] = inputs.pop(arg).output
+        for hint, arg in zip(hints, args):
+            if isinstance(hint, Fresh):
+                fresh.add(arg.name)
+                inputs[arg.name] = arg.output
+        fresh = frozenset(fresh)
+        Funsor.__init__(self, inputs, output, fresh, bound)
+        for name, arg in zip(self._ast_fields, args):
+            setattr(self, name, arg)
+
+    ResultMeta.__name__ = f"{fn.__name__}Meta"
+    Result = ResultMeta(fn.__name__, (Funsor,), {"__init__": __init__})
+    pattern = (Result,) + (Funsor,) * len(input_types)
+    eager.register(*pattern)(_erase_types(fn))
+    return Result
---
+++ test/test_factory.py
@@ -0,0 +1,90 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+from collections import OrderedDict
+
+import pytest
+
+from funsor.domains import Array, Bint, Real, Reals
+from funsor.factory import Bound, Fresh, make_funsor, to_funsor
+from funsor.terms import Cat, Funsor, Lambda
+from funsor.testing import check_funsor, random_tensor
+
+
+def test_lambda_lambda():
+    @make_funsor
+    def LambdaLambda(
+        i: Bound,
+        j: Bound,
+        x: Funsor,
+    ) -> Fresh[lambda i, j, x: Array[x.dtype, (i.size, j.size) + x.shape]]:
+        assert i in x.inputs
+        assert j in x.inputs
+        return Lambda(i, Lambda(j, x))
+
+
+@pytest.mark.parametrize("num_inputs", [0, 1, 2])
+def test_getitem_getitem(num_inputs):
+    @make_funsor
+    def GetitemGetitem(
+        x: Funsor,
+        i: Fresh[lambda x: Bint[x.shape[0]]],
+        j: Fresh[lambda x: Bint[x.shape[1]]],
+    ) -> Fresh[lambda x: Array[x.dtype, x.shape[2:]]]:
+        return x[i][j]
+
+    inputs = OrderedDict()
+    for name in "abc"[:num_inputs]:
+        inputs[name] = Bint[2]
+    x = random_tensor(inputs, Reals[3, 4])
+
+    expected = x["i"]["j"]
+    actual = GetitemGetitem(x, "i", "j")
+    check_funsor(actual, expected.inputs, expected.output, expected.data)
+
+
+@pytest.mark.xfail(reason="missing pattern Variable // Number")
+def test_flatten():
+    @make_funsor
+    def Flatten21(
+        x: Funsor,
+        i: Bound,
+        j: Bound,
+        ij: Fresh[lambda x, i, j: Bint[i.size * j.size]],
+    ) -> Fresh[lambda x, i, j: x.dtype]:
+        m = to_funsor(i, x.inputs.get(i, None)).output.size
+        n = to_funsor(j, x.inputs.get(j, None)).output.size
+        ij = to_funsor(ij, Bint[m * n])
+        return x(**{i: ij // n, j: ij % n})
+
+    inputs = OrderedDict()
+    inputs["a"] = Bint[3]
+    inputs["b"] = Bint[4]
+    data = random_tensor(inputs, Real)
+    x = Flatten21(data, "a", "b", "ab")
+
+    check_funsor(x, {"ab": Bint[12]}, Real, data.data.reshape(-1))
+
+
+def test_cat2():
+    @make_funsor
+    def Cat2(
+        x: Funsor,
+        y: Funsor,
+        i: Bound,
+        j: Bound,
+        ij: Fresh[lambda x, y, i, j: Bint[i.size + j.size]],
+    ) -> Fresh[lambda x, y, i, j: x]:
+        y = y(**{j.name: i})
+        result = Cat(i.name, (x, y))
+        result = result(**{i.name: ij})
+        return result
+
+    inputs = OrderedDict()
+    inputs["a"] = Bint[3]
+    inputs["b"] = Bint[4]
+    x = random_tensor(inputs, Real)
+    y = random_tensor(inputs, Real)
+    xy = Cat2(x, y, "a", "a", "aa")
+
+    check_funsor(xy, {"aa": Bint[6], "b": Bint[4]}, Real)
