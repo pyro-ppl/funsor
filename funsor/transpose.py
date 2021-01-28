4a25b2b9ca4954309df75638d9fbf28c5d465186
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-01-28 18:38:24 -0500

    Sketch Semiring class

--- funsor/transpose.py
+++ funsor/transpose.py
@@ -1,6 +1,8 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
+import math
+import contextlib
 from collections import defaultdict
 from functools import singledispatch
 
@@ -9,9 +11,53 @@ from funsor.cnf import Contraction
 from funsor.terms import Binary, Number, Reduce
 
 
+class Semiring:
+    @staticmethod
+    def backward_reduce(expr):
+        return transpose(expr.arg)
+
+
+class SumProduct(Semiring):
+    sum_op = ops.add
+    prod_op = ops.mul
+    div_op = ops.safediv
+    zero = Number(0)
+    one = Number(1)
+
+
+class Tropical(Semiring):
+    sum_op = ops.max
+    prod_op = ops.add
+    div_op = ops.safesub
+    zero = Number(-math.inf)
+    one = Number(0)
+
+    def backward_reduce(self, expr):
+        # TODO these should be a funsor pattern
+        raise NotImplementedError
+
+
+SEMIRING = SumProduct
+
+
+@contextlib.contextmanager
+def set_semiring(new):
+    assert isinstance(new, Semiring)
+    global SEMIRING
+    old = SEMIRING
+    try:
+        SEMIRING = new
+        yield
+    finally:
+        SEMIRING = old
+
+
 @singledispatch
 def transpose(expr):
-    result = defaultdict(lambda: Number(0))
+    """
+    Similar to adjoint, but currently hard-coded to sum-product semiring.
+    """
+    result = defaultdict(lambda: SEMIRING.zero)
     result[expr] = expr
     return result
 
@@ -20,8 +66,8 @@ def transpose(expr):
 def _(expr):
     lhs_adj = transpose(expr.lhs)
     rhs_adj = transpose(expr.rhs)
-    result = defaultdict(lambda: Number(0))
-    if expr.op is ops.add:
+    result = defaultdict(lambda: SEMIRING.zero)
+    if expr.op is SEMIRING.sum_op:
         for key, value in rhs_adj.items():
             result[key] += value
         for key, value in lhs_adj.items():
@@ -33,16 +79,25 @@ def _(expr):
         for key, value in lhs_adj.items():
             result[key] += value * expr.rhs
         return result
+    if isinstance(expr.op, ops.GetitemOp):
+        for key, value in transpose(expr.lhs).items():
+            result[key] += expr.op(value, expr.rhs)
+        return result
     raise NotImplementedError
 
 
 @transpose.register(Reduce)
 def _(expr):
-    arg_adj = transpose(expr.arg)
     if expr.op is ops.add:
-        return arg_adj
+        # Version 0.
+        return transpose(expr.arg)
+        # Version 1.
+        # return SEMIRING.backward_reduce(expr)
+        # Version 2.
+        # return Argreduce(SEMIRING, expr.arg, expr.reduced_vars)
     if expr.op is ops.mul:
-        result = defaultdict(lambda: Number(0))
+        arg_adj = transpose(expr.arg)
+        result = defaultdict(lambda: SEMIRING.zero)
         for k, v in arg_adj.values():
             result[k] = ops.safediv(
                 v * expr.arg, expr.arg.reduce(ops.mul, expr.reduced_vars))
@@ -57,7 +112,7 @@ def _(expr):
         term, = expr.terms
         return transpose(term)
     if expr.bin_op is ops.add and expr.red_op is ops.nullop:
-        result = defaultdict(lambda: Number(0))
+        result = defaultdict(lambda: SEMIRING.zero)
         for term in expr.terms:
             for key, value in transpose(term).items():
                 result[key] += value
@@ -67,7 +122,7 @@ def _(expr):
         lhs, rhs = expr.terms
         lhs_adj = transpose(lhs)
         rhs_adj = transpose(rhs)
-        result = defaultdict(lambda: Number(0))
+        result = defaultdict(lambda: SEMIRING.zero)
         for key, value in lhs_adj.items():
             result[key] += value * rhs
         for key, value in rhs_adj.items():
--- test/test_transpose.py
+++ test/test_transpose.py
@@ -1,12 +1,16 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
-from funsor.domains import Real
-from funsor.terms import Variable
-from funsor.testing import assert_close
-from funsor.transpose import transpose
+from collections import OrderedDict
 
 import pytest
+
+import funsor.ops as ops
+from funsor.domains import Bint, Real, Reals
+from funsor.interpreter import interpretation
+from funsor.terms import Variable, lazy
+from funsor.testing import assert_close, random_tensor
+from funsor.transpose import transpose
 
 
 @pytest.mark.parametrize("f", ["x", "y", "x + y", "1 + x * y", "2 * x - y"])
@@ -31,7 +35,7 @@ def test_product_rule(f, g):
 
 @pytest.mark.parametrize("f", ["x", "y", "x + y", "1 + x * y", "2 * x - y"])
 @pytest.mark.parametrize("g", ["x", "y", "x + y", "1 + x * y", "2 * x - y"])
-def test_sum_rule(f, g):
+def test_binary_sum_rule(f, g):
     x = Variable("x", Real)
     y = Variable("y", Real)
     f = eval(f)
@@ -47,3 +51,38 @@ def test_sum_rule(f, g):
         if actual is not expected:
             for x_, y_ in [(0., 0.1), (-1.3, 0.2), (0.3, 1.5)]:
                 assert_close(actual(x=x_, y=y_), expected(x=x_, y=y_))
+
+
+@pytest.mark.xfail(reason="probably an alpha conversion issue")
+def test_reduce_sum_rule():
+    i = Variable("i", Bint[4])
+    f = random_tensor(OrderedDict(i=Bint[4]))
+    with interpretation(lazy):
+        f_ = f.reduce(ops.add, i)
+
+    actual = transpose(f_)[f]
+    expected = transpose(f)[f]
+    print(f"actual:\n{str(actual)}")
+    print(f"expected:\n{str(expected)}")
+    assert actual.input_vars <= expected.input_vars
+    assert actual.output == expected.output
+    if actual is not expected:
+        for x_, y_ in [(0., 0.1), (-1.3, 0.2), (0.3, 1.5)]:
+            assert_close(actual(x=x_, y=y_), expected(x=x_, y=y_))
+
+
+@pytest.mark.xfail(reason="TODO reverse alpha renaming")
+def test_reduce_sum_getitem():
+    x = Variable("x", Reals[4])
+    i = Variable("i", Bint[4])
+    f = x[i]
+
+    actual = transpose(f.reduce(ops.add, i))[x]
+    expected = transpose(f)[x]
+    print(f"actual:\n{str(actual)}")
+    print(f"expected:\n{str(expected)}")
+    assert actual.input_vars <= expected.input_vars
+    assert actual.output == expected.output
+    if actual is not expected:
+        for x_, y_ in [(0., 0.1), (-1.3, 0.2), (0.3, 1.5)]:
+            assert_close(actual(x=x_, y=y_), expected(x=x_, y=y_))
