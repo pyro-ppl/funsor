9639398e02e3367821a4c3b9f9064bb9c98ca95a
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-02-02 19:03:46 -0500

    Another attempt

--- funsor/torch/__init__.py
+++ funsor/torch/__init__.py
@@ -4,14 +4,15 @@
 import torch
 from multipledispatch import dispatch
 
-import funsor.torch.distributions as _
-import funsor.torch.ops as _
 from funsor.adjoint import adjoint_ops
 from funsor.interpreter import children, recursion_reinterpret
 from funsor.ops import AssociativeOp
 from funsor.tensor import Tensor, tensor_to_funsor
 from funsor.terms import Funsor, to_funsor
 from funsor.util import quote
+
+from . import distributions as _
+from . import ops as _
 
 del _  # flake8
 
