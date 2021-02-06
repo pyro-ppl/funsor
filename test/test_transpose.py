c4efe5102f0f4051f89c69d353cb9224849f5be0
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-02-05 20:08:31 -0500

    Add more (failing) tests

--- test/test_transpose.py
+++ test/test_transpose.py
@@ -14,6 +14,121 @@ from funsor.testing import assert_close, random_tensor
 from funsor.transpose import transpose
 
 
+def test_identity():
+    i = Variable("i", Bint[4])
+    x = random_tensor(OrderedDict(i=Bint[4]))
+    assert transpose(x)[x] is Number(1.0)
+
+
+def test_two():
+    i = Variable("i", Bint[4])
+    x = random_tensor(OrderedDict(i=Bint[4]))
+    with interpretation(lazy):
+        y = x + x
+    assert transpose(y)[x] is Number(2.0)
+    assert transpose(y)[y] is Number(1.0)
+
+
+def test_zero():
+    i = Variable("i", Bint[4])
+    x = random_tensor(OrderedDict(i=Bint[4]))
+    with interpretation(lazy):
+        y = x - x
+    assert transpose(y)[x] is Number(0.0)
+    assert transpose(y)[y] is Number(1.0)
+
+
+def test_four():
+    i = Variable("i", Bint[4])
+    x = random_tensor(OrderedDict(i=Bint[4]))
+    with interpretation(lazy):
+        y = x + x
+        z = y + y
+    assert transpose(z)[x] is Number(4.0)
+    assert transpose(z)[y] is Number(2.0)
+    assert transpose(z)[z] is Number(1.0)
+
+
+def test_four_variable():
+    x = Variable("x", Real)
+    y = x + x
+    z = y + y
+    assert transpose(z)[x] is Number(4.0)
+    assert transpose(z)[y] is Number(2.0)
+    assert transpose(z)[z] is Number(1.0)
+
+
+def test_matmul():
+    x = random_tensor(OrderedDict(i=Bint[3], j=Bint[4]))
+    y = random_tensor(OrderedDict(j=Bint[4], k=Bint[5]))
+    with interpretation(lazy):
+        xy = x * y
+        z = xy.reduce(ops.add, "j")
+    assert_close(transpose(z)[x], y.reduce(ops.add, "k"))
+    assert_close(transpose(z)[y], x.reduce(ops.add, "i"))
+    assert_close(transpose(z)[xy], Number(1.))
+
+
+def test_batched_matmul():
+    x = random_tensor(OrderedDict(i=Bint[3], j=Bint[4], b=Bint[2]))
+    y = random_tensor(OrderedDict(j=Bint[4], k=Bint[5], b=Bint[2]))
+    with interpretation(lazy):
+        xy = x * y
+        z = xy.reduce(ops.add, "j")
+    assert_close(transpose(z)[x], y.reduce(ops.add, {"k"}))
+    assert_close(transpose(z)[y], x.reduce(ops.add, {"i"}))
+    assert_close(transpose(z)[xy], Number(1.))
+
+
+def test_expand_reduce():
+    i = Variable("i", Bint[3])
+    x = Variable("x", Real)
+    y = Variable("y", Real)
+    with interpretation(lazy):
+        xy = x * y
+        z = xy.reduce(ops.add, i)
+    assert_close(transpose(z)[x], y * 3)
+    assert_close(transpose(z)[y], x * 3)
+    assert_close(transpose(z)[xy], Number(1.))
+
+
+def test_tensor_contract():
+    x = random_tensor(OrderedDict(i1=Bint[2], i2=Bint[3], j1=Bint[4], j2=Bint[5]))
+    y = random_tensor(OrderedDict(j1=Bint[4], j2=Bint[5], k1=Bint[6], k2=Bint[7]))
+    with interpretation(lazy):
+        xy = x * y
+        z = xy.reduce(ops.add, {"j1", "j2"})
+    assert_close(transpose(z)[x], y.reduce(ops.add, {"k1", "k2"}))
+    assert_close(transpose(z)[y], x.reduce(ops.add, {"i1", "i2"}))
+    assert_close(transpose(z)[xy], Number(1.))
+
+
+@pytest.mark.parametrize("height", [1, 2, 3, 10, 100, 1000])
+def test_tower_sum(height):
+    x = random_tensor(OrderedDict(i=Bint[2], j=Bint[3]))
+    with interpretation(lazy):
+        top = x
+        for _ in range(height):
+            top = top + top
+    assert transpose(top)[x] is Number(2.0 ** height)
+
+
+@pytest.mark.parametrize("height", [1, 2, 3, 10, 100, 1000])
+def test_tower_prod(height):
+    x = random_tensor(OrderedDict(i=Bint[2], j=Bint[3]))
+    with interpretation(lazy):
+        top = x
+        expected = x
+        for _ in range(height):
+            top = top * top
+            expected = expected + expected
+        # This might be too much to ask:
+        transpose(top)[x] is expected
+
+    # This should definitely hold:
+    assert_close(transpose(top)[x], x * 2 ** height)
+
+
 @pytest.mark.parametrize("f", ["x", "y", "x + y", "1 + x * y", "2 * x - y"])
 @pytest.mark.parametrize("g", ["x", "y", "x + y", "1 + x * y", "2 * x - y"])
 def test_product_rule(f, g):
@@ -62,7 +177,7 @@ def test_reduce_sum_rule():
         f_ = f.reduce(ops.add, i)
 
     actual = transpose(f_)[f]
-    expected = transpose(f)[f]
+    expected = f
     print(f"actual:\n{str(actual)}")
     print(f"expected:\n{str(expected)}")
     assert actual.input_vars <= expected.input_vars
@@ -89,54 +204,26 @@ def test_reduce_sum_getitem():
             assert_close(actual(x=x_, y=y_), expected(x=x_, y=y_))
 
 
-@pytest.mark.xfail(reason="missing behavior")
-def test_backward_marginal():
-    """
-    This tests the following ideas:
+"""
+TODO tests the following ideas:
 
-      i:Bint[3] |- x:Real
-    -----------------------
-    |- x.reduce(op, i):Real
+  i:Bint[3] |- x:Real
+-----------------------
+|- x.reduce(op, i):Real
 
-               i:Bint[3] |- x:Real
-    ----------------------------------------------------
-    i2:Bint[3] |- transpose(x.reduce(op, i, i2))[x]:Real
+           i:Bint[3] |- x:Real
+----------------------------------------------------
+i2:Bint[3] |- transpose(x.reduce(op, i, i2))[x]:Real
 
-       G1 |- x1:t1   ...   Gn |- xn:tn
-          G0 |- f(x1, ..., xn):Real
-            f structurally linear
-    --------------------------------------
-    Gi |- transpose(F(x1, ..., xn))[xi]:ti
+   G1 |- x1:t1   ...   Gn |- xn:tn
+      G0 |- f(x1, ..., xn):Real
+        f structurally linear
+--------------------------------------
+Gi |- transpose(F(x1, ..., xn))[xi]:ti
 
-    G1 |- x:t1
-    G2 |- y = f(x) : t2
-    f structurally linear
-    --------------------------------------
-    G2 |- transpose(transpose(y)) = y : t2
-    """
-    i = Variable("i", Bint[3])
-    j = Variable("j", Bint[4])
-    x = random_tensor(OrderedDict(i=i.output, j=j.output))
-
-    # A forward computation.
-    with interpretation(lazy):
-        y = x.reduce(ops.logaddexp, j)
-
-    # Its transpose: backward marginal.
-    x2 = transpose(y)[x]
-    assert x2.inputs == x.inputs
-    assert x2.output == x.output
-
-    # Now in a different interpretation.
-    # FIXME can we sample eagerly but substitute lazily?
-    with interpretation(lazy), MonteCarlo():
-        y2 = x.reduce(ops.logaddexp, j)
-
-    # monte carlo sampling
-    x3 = transpose(y2)[x]
-    assert x3.inputs == x.inputs
-    assert x3.output == x.output
-
-    # monte carlo integration
-    y3 = x3.reduce(ops.logaddexp, j)
-    assert_close(y2, y3)
+G1 |- x:t1
+G2 |- y = f(x) : t2
+f structurally linear
+--------------------------------------
+G2 |- transpose(transpose(y)) = y : t2
+"""
