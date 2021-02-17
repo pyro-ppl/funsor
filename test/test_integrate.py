39b23e9562626657c4623a82ca5bb7765d7d9775
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-02-16 20:48:58 -0500

    Revert "Add a failing test"
    This reverts commit 846fe1502d9debe4422ba6e41a65dd782978478b.

--- test/test_integrate.py
+++ test/test_integrate.py
@@ -7,15 +7,11 @@ import numpy as np
 import pytest
 
 from funsor import ops
-from funsor.cnf import Contraction
-from funsor.domains import Bint, Real
-from funsor.gaussian import Gaussian
+from funsor.domains import Bint
 from funsor.integrate import Integrate
 from funsor.interpretations import eager, lazy, moment_matching, normalize, reflect
-from funsor.interpreter import reinterpret
 from funsor.montecarlo import MonteCarlo
-from funsor.tensor import Tensor
-from funsor.terms import Unary, Variable
+from funsor.terms import Variable
 from funsor.testing import assert_close, random_tensor
 
 
@@ -48,67 +44,3 @@ def test_syntactic_sugar():
     assert_close(Integrate(log_measure, integrand, i), expected)
     assert_close(Integrate(log_measure, integrand, {i}), expected)
     assert_close(Integrate(log_measure, integrand, frozenset([i])), expected)
-
-
-def test_gaussian_integrate_pattern():
-    torch = pytest.importorskip("torch")
-    with reflect:
-        x = Contraction(
-            ops.add,
-            ops.mul,
-            frozenset({Variable("x__BOUND_11", Real)}),
-            (
-                Unary(
-                    ops.exp,
-                    Gaussian(
-                        torch.tensor([0.0], dtype=torch.float32),
-                        torch.tensor([[1.0]], dtype=torch.float32),
-                        (
-                            (
-                                "x__BOUND_11",
-                                Real,
-                            ),
-                        ),
-                    ),
-                ),
-                Contraction(
-                    ops.nullop,
-                    ops.add,
-                    frozenset(),
-                    (
-                        Tensor(
-                            torch.tensor(
-                                [
-                                    0.9189385175704956,
-                                    0.9189385175704956,
-                                    0.9189385175704956,
-                                ],
-                                dtype=torch.float32,
-                            ),  # noqa
-                            (
-                                (
-                                    "plate_outer__BOUND_12",
-                                    Bint[
-                                        3,
-                                    ],
-                                ),
-                            ),
-                            "real",
-                        ),
-                        Gaussian(
-                            torch.tensor([-0.0], dtype=torch.float32),
-                            torch.tensor([[-1.0]], dtype=torch.float32),
-                            (
-                                (
-                                    "x__BOUND_11",
-                                    Real,
-                                ),
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        )
-
-    y = reinterpret(x)
-    assert isinstance(y, Tensor)
