6bc49a2d7f1c4be515d8ec9c90c9b9875c321193
Yerdos Ordabayev <ordabayev@brandeis.edu>
2021-02-24 18:35:05 -0500

    Tangent space

--- test/test_jvp.py
+++ test/test_jvp.py
@@ -3,27 +3,16 @@
 
 from collections import OrderedDict
 
-import pytest
-
-import funsor.jvp
 import funsor.ops as ops
-from funsor.domains import Bint, Real, Reals
-from funsor.interpreter import interpretation
-from funsor.optimizer import apply_optimizer
-from funsor.tensor import Tensor
-from funsor.terms import Number, Tuple, Variable, lazy, reflect
+from funsor.domains import Bint
+from funsor.jvp import Tangent
 from funsor.testing import assert_close, random_tensor
-
-try:
-    import torch
-except ImportError:
-    pytest.skip()
 
 
 def test_identity():
     x = random_tensor(OrderedDict(i=Bint[2]))
     dx = random_tensor(OrderedDict(i=Bint[2]))
-    x_ = Tuple((x, dx))
+    x_ = Tangent((x, dx))
     f, df = x_
     assert_close(f, x)
     assert_close(df, dx)
@@ -32,7 +21,7 @@ def test_identity():
 def test_log():
     x = random_tensor(OrderedDict(i=Bint[2]))
     dx = random_tensor(OrderedDict(i=Bint[2]))
-    x_ = Tuple((x, dx))
+    x_ = Tangent((x, dx))
     f, df = x_.log()
     assert_close(f, x.log())
     assert_close(df, dx / x)
@@ -45,25 +34,10 @@ def test_add():
     dy = random_tensor(OrderedDict(j=Bint[3]))
     # dx = Number(1.0)  # Variable("dx", Real)
     # dy = Number(1.0)  # Variable("dy", Real)
-    x_ = Tuple((x, dx))
-    y_ = Tuple((y, dy))
+    x_ = Tangent((x, dx))
+    y_ = Tangent((y, dy))
     f_ = x_ + y_
     f, df = f_
-    assert_close(f, x + y)
-    assert_close(df, dx + dy)
-
-
-def test_linearize_add():
-    x = random_tensor(OrderedDict(i=Bint[2]))
-    y = random_tensor(OrderedDict(j=Bint[3]))
-    dx = Variable("dx", Real)
-    dy = random_tensor(OrderedDict(j=Bint[3]))
-    # dy = Number(1.0)  # Variable("dy", Real)
-    x_ = Tuple((x, dx))
-    y_ = Tuple((y, dy))
-    f_ = x_ + y_
-    f, df = f_
-    breakpoint()
     assert_close(f, x + y)
     assert_close(df, dx + dy)
 
@@ -75,8 +49,8 @@ def test_mul():
     dy = random_tensor(OrderedDict(j=Bint[3]))
     # dx = Number(1.0)  # Variable("dx", Real)
     # dy = Number(0.0)  # Variable("dy", Real)
-    x_ = Tuple((x, dx))
-    y_ = Tuple((y, dy))
+    x_ = Tangent((x, dx))
+    y_ = Tangent((y, dy))
     f, df = x_ * y_
     assert_close(f, x * y)
     assert_close(df, (x * dy + y * dx).align(tuple(df.inputs.keys())))
@@ -85,18 +59,8 @@ def test_mul():
 def test_reduce_sum():
     x = random_tensor(OrderedDict(j=Bint[4]))
     dx = random_tensor(OrderedDict(j=Bint[4]))
-    x_ = Tuple((x, dx))
+    x_ = Tangent((x, dx))
     f, df = x_.reduce(ops.add, "j")
-    assert_close(f, x.reduce(ops.add, "j"))
-    assert_close(apply_optimizer(df), dx.reduce(ops.add, "j"))
-
-
-def test_linearize_reduce_sum():
-    x = random_tensor(OrderedDict(j=Bint[4]))
-    dx = random_tensor(OrderedDict(j=Bint[4]))
-    x_ = Tuple((x, dx))
-    f, df = x_.reduce(ops.add, "j")
-    breakpoint()
     assert_close(f, x.reduce(ops.add, "j"))
     assert_close(df, dx.reduce(ops.add, "j"))
 
@@ -104,7 +68,7 @@ def test_linearize_reduce_sum():
 def test_reduce_prod():
     x = random_tensor(OrderedDict(j=Bint[4]))
     dx = random_tensor(OrderedDict(j=Bint[4]))
-    x_ = Tuple((x, dx))
+    x_ = Tangent((x, dx))
     f, df = x_.reduce(ops.mul, "j")
     assert_close(f, x.reduce(ops.mul, "j"))
     assert_close(df, (f * dx / x).reduce(ops.add, "j"))
@@ -115,8 +79,8 @@ def test_matmul_tensor():
     y = random_tensor(OrderedDict(j=Bint[4], k=Bint[5]))
     dx = random_tensor(OrderedDict(j=Bint[4]))
     dy = random_tensor(OrderedDict(j=Bint[4], k=Bint[5]))
-    x_ = Tuple((x, dx))
-    y_ = Tuple((y, dy))
+    x_ = Tangent((x, dx))
+    y_ = Tangent((y, dy))
     xy_ = x_ * y_
     z, dz = xy_.reduce(ops.add, "j")
     assert_close(z, (x * y).reduce(ops.add, "j"))
@@ -128,9 +92,9 @@ def test_compose():
     y = random_tensor(OrderedDict(j=Bint[4], k=Bint[5]))
     dx = random_tensor(OrderedDict(j=Bint[4]))
     dy = random_tensor(OrderedDict(j=Bint[4], k=Bint[5]))
-    x_ = Tuple((x, dx))
+    x_ = Tangent((x, dx))
     logx_ = x_.log()
-    y_ = Tuple((y, dy))
+    y_ = Tangent((y, dy))
     logxy_ = logx_ * y_
     z, dz = logxy_.reduce(ops.add, "j")
     assert_close(z, (x.log() * y).reduce(ops.add, "j"))
