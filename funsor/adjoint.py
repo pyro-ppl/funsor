c0c5ea8c8ca9475d533877f809fdca3f8fbc50f2
Eli <eli@elibingham.com>
2021-03-01 19:55:01 -0500

    remove dead code

--- funsor/adjoint.py
+++ funsor/adjoint.py
@@ -1,26 +1,21 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
-from collections import OrderedDict, defaultdict
+from collections import defaultdict
 
-from funsor.cnf import Contraction, GaussianMixture, nullop
-from funsor.domains import Bint
-from funsor.gaussian import Gaussian, align_gaussian
+from funsor.cnf import Contraction, nullop
 from funsor.interpretations import Interpretation, reflect
 from funsor.interpreter import stack_reinterpret
 from funsor.ops import AssociativeOp
 from funsor.registry import KeyedRegistry
-from funsor.tensor import Tensor
 from funsor.terms import (
     Binary,
     Cat,
     Funsor,
-    Number,
     Reduce,
     Scatter,
     Slice,
     Subs,
-    Variable,
     substitute,
     to_funsor,
 )
@@ -283,256 +278,7 @@ def adjoint_subs(adj_sum_op, adj_prod_op, out_adj, arg, subs):
     AssociativeOp,
     tuple,
     Funsor,
+    frozenset,
 )
-def adjoint_scatter(adj_sum_op, adj_prod_op, out_adj, op, subs, source):
-    return ((source, out_adj(**dict(subs))),)
-
-
-def _scatter(src, res, subs):
-    # inverse of advanced indexing
-    # TODO check types of subs, in case some logic from eager_subs was accidentally left out?
-
-    # use advanced indexing logic copied from Tensor.eager_subs:
-
-    # materialize after checking for renaming case
-    subs = OrderedDict((k, res.materialize(v)) for k, v in subs)
-
-    # Compute result shapes.
-    inputs = OrderedDict()
-    for k, domain in res.inputs.items():
-        inputs[k] = domain
-
-    # Construct a dict with each input's positional dim,
-    # counting from the right so as to support broadcasting.
-    total_size = len(inputs) + len(res.output.shape)  # Assumes only scalar indices.
-    new_dims = {}
-    for k, domain in inputs.items():
-        assert not domain.shape
-        new_dims[k] = len(new_dims) - total_size
-
-    # Use advanced indexing to construct a simultaneous substitution.
-    index = []
-    for k, domain in res.inputs.items():
-        if k in subs:
-            v = subs.get(k)
-            if isinstance(v, Number):
-                index.append(int(v.data))
-            else:
-                # Permute and expand v.data to end up at new_dims.
-                assert isinstance(v, Tensor)
-                v = v.align(tuple(k2 for k2 in inputs if k2 in v.inputs))
-                assert isinstance(v, Tensor)
-                v_shape = [1] * total_size
-                for k2, size in zip(v.inputs, v.data.shape):
-                    v_shape[new_dims[k2]] = size
-                index.append(v.data.reshape(tuple(v_shape)))
-        else:
-            # Construct a [:] slice for this preserved input.
-            offset_from_right = -1 - new_dims[k]
-            index.append(
-                ops.new_arange(res.data, domain.dtype).reshape(
-                    (-1,) + (1,) * offset_from_right
-                )
-            )
-
-    # Construct a [:] slice for the output.
-    for i, size in enumerate(res.output.shape):
-        offset_from_right = len(res.output.shape) - i - 1
-        index.append(
-            ops.new_arange(res.data, size).reshape((-1,) + (1,) * offset_from_right)
-        )
-
-    # the only difference from Tensor.eager_subs is here:
-    # instead of indexing the rhs (lhs = rhs[index]), we index the lhs (lhs[index] = rhs)
-
-    # unsqueeze to make broadcasting work
-    src_inputs, src_data = src.inputs.copy(), src.data
-    for k, v in res.inputs.items():
-        if k not in src.inputs and isinstance(subs[k], Number):
-            src_inputs[k] = Bint[1]
-            src_data = src_data.unsqueeze(-1 - len(src.output.shape))
-    src = Tensor(src_data, src_inputs, src.output.dtype).align(tuple(res.inputs.keys()))
-
-    # TODO refactor to scatter_add to support non-injective substitution
-    # TODO add and test ops.scatter_add
-    # data = ops.scatter_add(res.data, tuple(index), src.data)
-
-    data = res.data
-    data[tuple(index)] = src.data
-
-    return Tensor(data, inputs, res.dtype)
-
-
-@adjoint_ops.register(
-    Subs, ops.LogaddexpOp, ops.AddOp, GaussianMixture, GaussianMixture, tuple
-)
-def adjoint_subs_gaussianmixture_gaussianmixture(
-    adj_sum_op, adj_prod_op, out_adj, arg, subs
-):
-
-    if any(v.dtype == "real" and not isinstance(v, Variable) for k, v in subs):
-        raise NotImplementedError(
-            "TODO implement adjoint for substitution into Gaussian real variable"
-        )
-
-    # invert renaming
-    renames = tuple((v.name, k) for k, v in subs if isinstance(v, Variable))
-    out_adj = Subs(out_adj, renames)
-
-    # inverting advanced indexing
-    slices = tuple((k, v) for k, v in subs if not isinstance(v, Variable))
-
-    assert len(slices + renames) == len(subs)
-
-    in_adj_discrete = adjoint_ops(
-        Subs, adj_sum_op, adj_prod_op, out_adj.terms[0], arg.terms[0], subs
-    )[0][1]
-
-    arg_int_inputs = OrderedDict(
-        (k, v) for k, v in arg.inputs.items() if v.dtype != "real"
-    )
-    out_adj_int_inputs = OrderedDict(
-        (k, v) for k, v in out_adj.inputs.items() if v.dtype != "real"
-    )
-
-    arg_real_inputs = OrderedDict(
-        (k, v) for k, v in arg.inputs.items() if v.dtype == "real"
-    )
-
-    align_inputs = OrderedDict(
-        (k, v) for k, v in out_adj.terms[1].inputs.items() if v.dtype != "real"
-    )
-    align_inputs.update(arg_real_inputs)
-    out_adj_info_vec, out_adj_precision = align_gaussian(align_inputs, out_adj.terms[1])
-
-    in_adj_info_vec = adjoint_ops(
-        Subs,
-        adj_sum_op,
-        adj_prod_op,  # ops.add, ops.mul,
-        Tensor(out_adj_info_vec, out_adj_int_inputs),
-        Tensor(arg.terms[1].info_vec, arg_int_inputs),
-        slices,
-    )[0][1]
-
-    in_adj_precision = adjoint_ops(
-        Subs,
-        adj_sum_op,
-        adj_prod_op,  # ops.add, ops.mul,
-        Tensor(out_adj_precision, out_adj_int_inputs),
-        Tensor(arg.terms[1].precision, arg_int_inputs),
-        slices,
-    )[0][1]
-
-    assert isinstance(in_adj_info_vec, Tensor)
-    assert isinstance(in_adj_precision, Tensor)
-
-    in_adj_gaussian = Gaussian(
-        in_adj_info_vec.data, in_adj_precision.data, arg.inputs.copy()
-    )
-
-    in_adj = in_adj_gaussian + in_adj_discrete
-    return ((arg, in_adj),)
-
-
-@adjoint_ops.register(
-    Subs, ops.LogaddexpOp, ops.AddOp, Gaussian, GaussianMixture, tuple
-)
-def adjoint_subs_gaussianmixture_discrete(adj_sum_op, adj_prod_op, out_adj, arg, subs):
-
-    if any(v.dtype == "real" and not isinstance(v, Variable) for k, v in subs):
-        raise NotImplementedError(
-            "TODO implement adjoint for substitution into Gaussian real variable"
-        )
-
-    out_adj_int_inputs = OrderedDict(
-        (k, v) for k, v in out_adj.inputs.items() if v.dtype != "real"
-    )
-    out_adj_ = out_adj + Tensor(
-        out_adj.info_vec.new_zeros(out_adj.info_vec.shape[:-1]), out_adj_int_inputs
-    )
-    return adjoint_ops(Subs, adj_sum_op, adj_prod_op, out_adj_, arg, subs)
-
-
-@adjoint_ops.register(
-    Subs, ops.LogaddexpOp, ops.AddOp, (GaussianMixture, Gaussian), Gaussian, tuple
-)
-def adjoint_subs_gaussian_gaussian(adj_sum_op, adj_prod_op, out_adj, arg, subs):
-
-    if any(v.dtype == "real" and not isinstance(v, Variable) for k, v in subs):
-        raise NotImplementedError(
-            "TODO implement adjoint for substitution into Gaussian real variable"
-        )
-
-    arg_int_inputs = OrderedDict(
-        (k, v) for k, v in arg.inputs.items() if v.dtype != "real"
-    )
-    arg_ = arg + Tensor(arg.info_vec.new_zeros(arg.info_vec.shape[:-1]), arg_int_inputs)
-    return (
-        (arg, adjoint_ops(Subs, adj_sum_op, adj_prod_op, out_adj, arg_, subs)[0][1]),
-    )
-
-
-@adjoint_ops.register(
-    Subs, ops.LogaddexpOp, ops.AddOp, (Number, Tensor), GaussianMixture, tuple
-)
-def adjoint_subs_gaussianmixture_discrete(adj_sum_op, adj_prod_op, out_adj, arg, subs):
-
-    if any(v.dtype == "real" and not isinstance(v, Variable) for k, v in subs):
-        raise NotImplementedError(
-            "TODO implement adjoint for substitution into Gaussian real variable"
-        )
-
-    # invert renaming
-    renames = tuple((v.name, k) for k, v in subs if isinstance(v, Variable))
-    out_adj = Subs(out_adj, renames)
-
-    # inverting advanced indexing
-    slices = tuple((k, v) for k, v in subs if not isinstance(v, Variable))
-
-    arg_int_inputs = OrderedDict(
-        (k, v) for k, v in arg.inputs.items() if v.dtype != "real"
-    )
-
-    zeros_like_out = Subs(
-        Tensor(
-            arg.terms[1].info_vec.new_full(
-                arg.terms[1].info_vec.shape[:-1], ops.UNITS[adj_prod_op]
-            ),
-            arg_int_inputs,
-        ),
-        slices,
-    )
-    out_adj = adj_prod_op(out_adj, zeros_like_out)
-
-    in_adj_discrete = adjoint_ops(
-        Subs, adj_sum_op, adj_prod_op, out_adj, arg.terms[0], subs
-    )[0][1]
-
-    # invert the slicing for the Gaussian term even though the message does not affect the values
-    in_adj_info_vec = adjoint_ops(
-        Subs,
-        adj_sum_op,
-        adj_prod_op,  # ops.add, ops.mul,
-        zeros_like_out,
-        Tensor(arg.terms[1].info_vec, arg_int_inputs),
-        slices,
-    )[0][1]
-
-    in_adj_precision = adjoint_ops(
-        Subs,
-        adj_sum_op,
-        adj_prod_op,  # ops.add, ops.mul,
-        zeros_like_out,
-        Tensor(arg.terms[1].precision, arg_int_inputs),
-        slices,
-    )[0][1]
-
-    assert isinstance(in_adj_info_vec, Tensor)
-    assert isinstance(in_adj_precision, Tensor)
-
-    in_adj_gaussian = Gaussian(
-        in_adj_info_vec.data, in_adj_precision.data, arg.inputs.copy()
-    )
-
-    in_adj = in_adj_gaussian + in_adj_discrete
-    return ((arg, in_adj),)
+def adjoint_scatter(adj_sum_op, adj_prod_op, out_adj, op, subs, source, reduced_vars):
+    return ((source, out_adj(**dict(subs)).reduce(adj_sum_op, reduced_vars)),)
