665426932ae88aedc0e2ce47fc2c6bf92dead2ac
Yerdos Ordabayev <ordabayev@brandeis.edu>
2021-03-04 18:10:35 -0500

    address comments

--- examples/forward_backward.py
+++ examples/forward_backward.py
@@ -5,18 +5,14 @@ import argparse
 from collections import OrderedDict
 from typing import Dict, List, Tuple
 
-import funsor
 import funsor.ops as ops
 from funsor import Funsor, Tensor
 from funsor.adjoint import AdjointTape
 from funsor.domains import Bint
-from funsor.ops.builtin import UNITS, AssociativeOp
 from funsor.testing import assert_close, random_tensor
 
 
 def forward_algorithm(
-    sum_op: AssociativeOp,
-    prod_op: AssociativeOp,
     factors: List[Funsor],
     step: Dict[str, str],
 ) -> Tuple[Funsor, List[Funsor]]:
@@ -24,7 +20,7 @@ def forward_algorithm(
     Calculate log marginal probability using the forward algorithm:
     Z = p(y[0:T])
 
-    Transition and emission probabilities are given by init and trans:
+    Transition and emission probabilities are given by init and trans factors:
     init = p(y[0], x[0])
     trans[t] = p(y[t], x[t] | x[t-1])
 
@@ -44,18 +40,16 @@ def forward_algorithm(
     alphas = [alpha]
     # inductive steps
     for trans in factors[1:]:
-        alpha = prod_op(alpha(**curr_to_drop), trans(**prev_to_drop)).reduce(
-            sum_op, reduce_vars
+        alpha = (alpha(**curr_to_drop) + trans(**prev_to_drop)).reduce(
+            ops.logaddexp, reduce_vars
         )
         alphas.append(alpha)
     else:
-        Z = alpha(**curr_to_drop).reduce(sum_op, reduce_vars)
+        Z = alpha(**curr_to_drop).reduce(ops.logaddexp, reduce_vars)
     return Z
 
 
 def forward_backward_algorithm(
-    sum_op: AssociativeOp,
-    prod_op: AssociativeOp,
     factors: List[Funsor],
     step: Dict[str, str],
 ) -> List[Tensor]:
@@ -69,44 +63,39 @@ def forward_backward_algorithm(
     curr_to_drop = dict(zip(step.values(), drop))
     reduce_vars = frozenset(drop)
 
+    # Base cases
+    alpha = factors[0]  # alpha[0] = p(y[0], x[0])
+    beta = Tensor(
+        ops.full_like(alpha.data, 0.0), alpha(x_curr="x_prev").inputs
+    )  # beta[T] = 1
+
     # Backward algorithm
-    # beta[t] = p(y[t+1:T]|x[t])
-    # beta[T] = 1
-    data = ops.full_like(factors[0].data, UNITS[prod_op])
-    beta = Tensor(data, factors[0](x_curr="x_prev").inputs, factors[0].dtype)
-    betas = [beta]
-    # inductive steps
+    # beta[t] = p(y[t+1:T] | x[t])
     # beta[t] = trans[t+1] @ beta[t+1]
+    betas = [beta]
     for trans in factors[:0:-1]:
-        beta = prod_op(trans(**curr_to_drop), beta(**prev_to_drop)).reduce(
-            sum_op, reduce_vars
+        beta = (trans(**curr_to_drop) + beta(**prev_to_drop)).reduce(
+            ops.logaddexp, reduce_vars
         )
         betas.append(beta)
     else:
         init = factors[0]
-        Z = prod_op(init(**curr_to_drop), beta(**prev_to_drop)).reduce(
-            sum_op, reduce_vars
+        Z = (init(**curr_to_drop) + beta(**prev_to_drop)).reduce(
+            ops.logaddexp, reduce_vars
         )
     betas.reverse()
 
-    # Forward algorithm
-    # p(y[0], x[0])
-    alpha = factors[0]
-    # p(x[0] | Y)
-    marginal = ops.BINARY_INVERSES[prod_op](
-        prod_op(alpha, betas[0](**{"x_prev": "x_curr"})), Z
-    )
+    # Forward algorithm & Marginal computations
+    marginal = alpha + betas[0](**{"x_prev": "x_curr"}) - Z  # p(x[0] | Y)
     marginals = [marginal]
     # inductive steps
     for trans, beta in zip(factors[1:], betas[1:]):
-        # p(y[0:t], x[t-1], x[t])
-        alpha_trans = prod_op(alpha(**{"x_curr": "x_prev"}), trans)
-        # p(y[0:t], x[t])
-        alpha = alpha_trans.reduce(sum_op, frozenset({"x_prev"}))
-        # p(x[t-1], x[t] | Y)
-        marginal = ops.BINARY_INVERSES[prod_op](
-            prod_op(alpha_trans, beta(**{"x_prev": "x_curr"})), Z
-        )
+        # alpha[t-1] * trans[t] = p(y[0:t], x[t-1], x[t])
+        alpha_trans = alpha(**{"x_curr": "x_prev"}) + trans
+        # alpha[t] = p(y[0:t], x[t])
+        alpha = alpha_trans.reduce(ops.logaddexp, frozenset({"x_prev"}))
+        # alpha[t-1] * trans[t] * beta[t] / Z = p(x[t-1], x[t] | Y)
+        marginal = alpha_trans + beta(**{"x_prev": "x_curr"}) - Z
         marginals.append(marginal)
 
     return marginals
@@ -141,9 +130,6 @@ def main(args):
         with Applications to Minimum-Risk Training on Translation Forests"
         http://www.cs.jhu.edu/~zfli/pubs/semiring_translation_zhifei_emnlp09.pdf
     """
-    funsor.set_backend("torch")
-    sum_op = ops.logaddexp  # ops.add
-    prod_op = ops.add  # ops.mul
 
     # transition and emission probabilities
     init = random_tensor(OrderedDict([("x_curr", Bint[args.hidden_dim])]))
@@ -161,20 +147,18 @@ def main(args):
         )
 
     # Compute marginal probabilities using the forward-backward algorithm
-    marginals = forward_backward_algorithm(
-        sum_op, prod_op, factors, {"x_prev": "x_curr"}
-    )
+    marginals = forward_backward_algorithm(factors, {"x_prev": "x_curr"})
     # Compute marginal probabilities using backpropagation
     with AdjointTape() as tape:
-        Z = forward_algorithm(sum_op, prod_op, factors, {"x_prev": "x_curr"})
-    result = tape.adjoint(sum_op, prod_op, Z, factors)
+        Z = forward_algorithm(factors, {"x_prev": "x_curr"})
+    result = tape.adjoint(ops.logaddexp, ops.add, Z, factors)
     adjoint_terms = list(result.values())
 
     t = 0
     for expected, adj, trans in zip(marginals, adjoint_terms, factors):
         # adjoint returns dZ / dtrans = alpha[t-1] * beta[t]
         # marginal = alpha[t-1] * beta[t] * trans / Z
-        actual = ops.BINARY_INVERSES[prod_op](prod_op(adj, trans), Z)
+        actual = adj + trans - Z
         assert_close(expected, actual.align(tuple(expected.inputs)), rtol=1e-4)
         print("")
         print(f"Marginal term: p(x[{t}], x[{t-1}] | Y)")
