bca62625817fb13f43221fcb8c5aa74453a4354f
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-03-29 20:03:32 -0400

    Fix tensor indentation

--- funsor/tensor.py
+++ funsor/tensor.py
@@ -151,7 +151,7 @@ class Tensor(Funsor, metaclass=TensorMeta):
 
     @ignore_jit_warnings()
     def __repr__(self):
-        data = repr(self.data)
+        data = str(self.data).replace("\n", "\n       ")
         inputs = dict.__repr__(self.inputs)
         if self.dtype != "real":
             return "Tensor({}, {}, {})".format(data, inputs, repr(self.dtype))
@@ -162,7 +162,7 @@ class Tensor(Funsor, metaclass=TensorMeta):
 
     @ignore_jit_warnings()
     def __str__(self):
-        data = str(self.data)
+        data = str(self.data).replace("\n", "\n       ")
         inputs = dict.__repr__(self.inputs)
         if self.dtype != "real":
             return "Tensor({}, {}, {})".format(data, inputs, repr(self.dtype))
