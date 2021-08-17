aceb7d2a16a2fcdb60b759f527197c714eb5cbe6
Yerdos Ordabayev <ordabayev@brandeis.edu>
2021-08-17 17:50:24 -0400

    remove Vindex test

--- funsor/torch/provenance.py
+++ funsor/torch/provenance.py
@@ -5,6 +5,12 @@ import torch
 
 
 class ProvenanceTensor(torch.Tensor):
+    """
+    Provenance tracking implementation in Pytorch.
+
+    Provenance of the output tensor is the union of provenances of input tensors.
+    """
+
     def __new__(cls, data, provenance=frozenset(), **kwargs):
         if not provenance:
             return data
@@ -26,7 +32,9 @@ class ProvenanceTensor(torch.Tensor):
     def __torch_function__(self, func, types, args=(), kwargs=None):
         if kwargs is None:
             kwargs = {}
+        # collect provenance information
         provenance = frozenset()
+        # extract tensor data ._t from ProvenanceTensor args
         _args = []
         for arg in args:
             if isinstance(arg, ProvenanceTensor):
@@ -44,16 +52,15 @@ class ProvenanceTensor(torch.Tensor):
             else:
                 _args.append(arg)
         ret = func(*_args, **kwargs)
-        if isinstance(ret, torch.Tensor):
-            if provenance:
+        if provenance:
+            if isinstance(ret, torch.Tensor):
                 return ProvenanceTensor(ret, provenance=provenance)
-            return ret
-        if isinstance(ret, tuple):
-            _ret = []
-            for r in ret:
-                if isinstance(r, torch.Tensor) and provenance:
-                    _ret.append(ProvenanceTensor(r, provenance=provenance))
-                else:
-                    _ret.append(r)
-            return tuple(_ret)
+            if isinstance(ret, tuple):
+                _ret = []
+                for r in ret:
+                    if isinstance(r, torch.Tensor):
+                        _ret.append(ProvenanceTensor(r, provenance=provenance))
+                    else:
+                        _ret.append(r)
+                return tuple(_ret)
         return ret
--- test/test_provenance.py
+++ test/test_provenance.py
@@ -3,7 +3,6 @@
 
 import pytest
 import torch
-from pyro.ops.indexing import Vindex
 
 from funsor.torch.provenance import ProvenanceTensor
 
@@ -48,7 +47,7 @@ def test_binary_add(data1, provenance1, data2, provenance2):
     if provenance2 is not None:
         data2 = ProvenanceTensor(data2, provenance2)
 
-    expected = frozenset.union(
+    expected = frozenset().union(
         *[m for m in (provenance1, provenance2) if m is not None]
     )
     actual = (data1 + data2)._provenance
@@ -79,42 +78,4 @@ def test_indexing(data1, provenance1, data2, provenance2):
         *[m for m in (provenance1, provenance2) if m is not None]
     )
     actual = getattr(data1[data2], "_provenance", frozenset())
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "data1,provenance1",
-    [
-        (torch.tensor([[0, 1], [2, 3]]), frozenset({"a"})),
-        (torch.tensor([[0, 1], [2, 3]]), None),
-    ],
-)
-@pytest.mark.parametrize(
-    "data2,provenance2",
-    [
-        (torch.tensor([0.0, 1.0]), frozenset({"b"})),
-        (torch.tensor([0.0, 1.0]), None),
-    ],
-)
-@pytest.mark.parametrize(
-    "data3,provenance3",
-    [
-        (torch.tensor([0, 1]), frozenset({"c"})),
-        (torch.tensor([0, 1]), None),
-    ],
-)
-def test_vindex(data1, provenance1, data2, provenance2, data3, provenance3):
-    if provenance1 is not None:
-        data1 = ProvenanceTensor(data1, provenance1)
-    if provenance2 is not None:
-        data2 = ProvenanceTensor(data2, provenance2)
-    if provenance3 is not None:
-        data3 = ProvenanceTensor(data3, provenance3)
-
-    expected = frozenset().union(
-        *[m for m in (provenance1, provenance2, provenance3) if m is not None]
-    )
-    actual = getattr(
-        Vindex(data1)[data2.long().unsqueeze(-1), data3], "_provenance", frozenset()
-    )
     assert actual == expected
