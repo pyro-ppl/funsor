9b3822169bdadb86875ef9a673cd9f439abd8047
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-09-21 16:11:58 -0400

    Add more tests (still failing)

--- test/test_recipes.py
+++ test/test_recipes.py
@@ -3,8 +3,10 @@
 
 from collections import OrderedDict
 
+import pytest
+
 import funsor.ops as ops
-from funsor.domains import Bint, Real
+from funsor.domains import Bint, Real, Reals
 from funsor.montecarlo import extract_samples
 from funsor.recipes import forward_filter_backward_rsample
 from funsor.testing import assert_close, random_gaussian
@@ -46,12 +48,13 @@ def test_ffbr_1():
     }
     eliminate = frozenset(["a", "b"])
     plates = frozenset()
-    sample_inputs = {"particles": Bint[num_samples]}
+    sample_inputs = {"particle": Bint[num_samples]}
+
     actual_samples, actual_log_prob = forward_filter_backward_rsample(
         factors, eliminate, plates, sample_inputs
     )
-    assert set(actual_samples.inputs) == {"a", "particles"}
-    actual_moments = get_moments(actual_samples, sample_inputs)
+    assert set(actual_samples) == {"a"}
+    assert set(actual_samples["a"].inputs) == {"particle"}
 
     # Check log_prob.
     guide = sum(factors.values())
@@ -61,8 +64,8 @@ def test_ffbr_1():
     # Check sample moments.
     joint = sum(factors.values())
     expected_samples = extract_samples(joint.sample(**sample_inputs))
-    assert set(expected_samples.inputs) == {"a", "particles"}
     expected_moments = get_moments(expected_samples, sample_inputs)
+    actual_moments = get_moments(actual_samples, sample_inputs)
     assert_close(actual_moments, expected_moments)
 
 
@@ -82,12 +85,13 @@ def test_ffbr_2():
     }
     eliminate = frozenset(["a", "b"])
     plates = frozenset()
-    sample_inputs = {"particles": Bint[num_samples]}
+    sample_inputs = {"particle": Bint[num_samples]}
     actual_samples, actual_log_prob = forward_filter_backward_rsample(
         factors, eliminate, plates, sample_inputs
     )
-    assert set(actual_samples.inputs) == {"a", "b", "particles"}
-    actual_moments = get_moments(actual_samples, sample_inputs)
+    assert set(actual_samples) == {"a", "b"}
+    assert set(actual_samples["a"].inputs) == {"particle"}
+    assert set(actual_samples["b"].inputs) == {"particle"}
 
     # Check log_prob.
     guide = sum(factors.values())
@@ -97,6 +101,80 @@ def test_ffbr_2():
     # Check sample moments.
     joint = sum(factors.values())
     expected_samples = extract_samples(joint.sample(**sample_inputs))
-    assert set(expected_samples.inputs) == {"a", "b", "particles"}
     expected_moments = get_moments(expected_samples, sample_inputs)
+    actual_moments = get_moments(actual_samples, sample_inputs)
+    assert_close(actual_moments, expected_moments)
+
+
+def test_ffbr_3():
+    """
+    def model(data):
+        a = pyro.sample("a", dist.Normal(0, 1))
+        with pyro.plate("plate", 2):
+            b = pyro.sample("b", dist.Normal(0, 1))
+            c = pyro.sample("c", dist.Normal(a, b.exp()), obs=data)
+    """
+    num_samples = 1000
+
+    factors = {
+        "a": random_gaussian(OrderedDict({"a": Real})),
+        "b": random_gaussian(OrderedDict({"plate": Bint[2], "b": Real})),
+        "c": random_gaussian(OrderedDict({"plate": Bint[2], "a": Real, "b": Real})),
+    }
+    eliminate = frozenset(["a", "b", "plate"])
+    plates = frozenset(["plate"])
+    sample_inputs = {"particle": Bint[num_samples]}
+    actual_samples, actual_log_prob = forward_filter_backward_rsample(
+        factors, eliminate, plates, sample_inputs
+    )
+    assert set(actual_samples) == {"a", "b"}
+    assert set(actual_samples["a"].inputs) == {"particle"}
+    assert set(actual_samples["b"].inputs) == {"plate", "particle"}
+
+    # Check log_prob.
+    guide = sum(factors.values())
+    expected_log_prob = guide(**actual_samples)
+    assert_close(actual_log_prob, expected_log_prob)
+
+    # Check sample moments.
+    joint = sum(factors.values())
+    expected_samples = extract_samples(joint.sample(**sample_inputs))
+    expected_moments = get_moments(expected_samples, sample_inputs)
+    actual_moments = get_moments(actual_samples, sample_inputs)
+    assert_close(actual_moments, expected_moments)
+
+
+@pytest.mark.xfail(reason="TODO handle colliders via Lambda")
+def test_ffbr_4():
+    """
+    def model(data):
+        with pyro.plate("plate", 2):
+            a = pyro.sample("a", dist.Normal(0, 1))
+        b = pyro.sample("b", dist.Normal(a.sum(), 1), obs=data)
+    """
+    num_samples = 1000
+
+    factors = {
+        "a": random_gaussian(OrderedDict({"plate": Bint[2], "a": Real})),
+        "b": random_gaussian(OrderedDict({"a_plate": Reals[2]})),
+    }
+    eliminate = frozenset(["a", "plate"])
+    plates = frozenset(["plate"])
+    sample_inputs = {"particle": Bint[num_samples]}
+    actual_samples, actual_log_prob = forward_filter_backward_rsample(
+        factors, eliminate, plates, sample_inputs
+    )
+    assert set(actual_samples) == {"a"}
+    assert set(actual_samples["a"].inputs) == {"plate", "particle"}
+
+    # Check log_prob.
+    guide = sum(factors.values())
+    expected_log_prob = guide(**actual_samples)
+    assert_close(actual_log_prob, expected_log_prob)
+
+    # Check sample moments.
+    joint = sum(factors.values())
+    expected_samples = extract_samples(joint.sample(**sample_inputs))
+    expected_moments = get_moments(expected_samples, sample_inputs)
+    actual_moments = get_moments(actual_samples, sample_inputs)
     assert_close(actual_moments, expected_moments)
