8fe5432dbdbac06eb0861977c971d9b31ceb5cf4
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-10-19 08:56:57 -0400

    Sketch forward_max_backward_argmax

--- funsor/recipes.py
+++ funsor/recipes.py
@@ -82,3 +82,67 @@ def forward_filter_backward_rsample(
     assert set(log_prob.inputs) == set(sample_inputs)
 
     return samples, log_prob
+
+
+def forward_max_backward_argmax(
+    factors: Dict[str, funsor.Funsor],
+    eliminate: FrozenSet[str],
+    plates: FrozenSet[str],
+    rng_key=None,
+):
+    """
+    A forward-filter backward-batched-reparametrized-sample algorithm for use
+    in variational inference. The motivating use case is performing Gaussian
+    tensor variable elimination over structured variational posteriors.
+
+    :param dict factors: A dictionary mapping sample site name to a Funsor
+        factor created at that sample site.
+    :param frozenset: A set of names of latent variables to marginalize and
+        plates to aggregate.
+    :param plates: A set of names of plates to aggregate.
+    :param rng_key: A random number key for the JAX backend.
+    :returns: A pair ``samples:Dict[str, Tensor], log_prob: Tensor`` of samples
+        and log density evaluated at each of those samples. If ``sample_inputs``
+        is nonempty, both outputs will be batched.
+    :rtype: tuple
+    """
+    assert isinstance(factors, dict)
+    assert all(isinstance(k, str) for k in factors)
+    assert all(isinstance(v, funsor.Funsor) for v in factors.values())
+    assert isinstance(eliminate, frozenset)
+    assert all(isinstance(v, str) for v in eliminate)
+    assert isinstance(plates, frozenset)
+    assert all(isinstance(v, str) for v in plates)
+
+    # Perform tensor variable elimination.
+    with funsor.interpretations.reflect:
+        log_Z = funsor.sum_product.sum_product(
+            funsor.ops.max,
+            funsor.ops.add,
+            list(factors.values()),
+            eliminate,
+            plates,
+        )
+        log_Z = funsor.optimizer.apply_optimizer(log_Z)
+    with funsor.approximations.argmax_approximate:
+        log_Z, marginals = funsor.adjoint.forward_backward(
+            funsor.ops.max, funsor.ops.add, log_Z
+        )
+
+    # Extract sample tensors.
+    samples = {}
+    for name, factor in factors.items():
+        if name in eliminate:
+            samples.update(funsor.montecarlo.extract_samples(marginals[factor]))
+    assert frozenset(samples) == eliminate - plates
+
+    # Compute log density at each sample.
+    log_prob = -log_Z
+    for f in factors.values():
+        term = f(**samples)
+        plates = eliminate.intersection(term.inputs)
+        term = term.reduce(funsor.ops.add, plates)
+        log_prob += term
+    assert not log_prob.inputs
+
+    return samples, log_prob
