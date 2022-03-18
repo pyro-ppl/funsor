bf49497b26e5cc2c311540f2f733eb7a95bdfdbe
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2022-03-18 14:41:29 -0400

    Bump to version 0.4.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = "\n".join(line for line in long_description.split("\n")[4:])
 
 setup(
     name="funsor",
-    version="0.4.2",
+    version="0.4.1",  # mirrored in funsor/__init__.py
     description="A tensor-like library for functions and distributions",
     packages=find_packages(include=["funsor", "funsor.*"]),
     url="https://github.com/pyro-ppl/funsor",
