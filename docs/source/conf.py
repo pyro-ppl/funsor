018bd5901e44fef2c830ca94c12611f53e072372
Du Phan <fehiepsi@gmail.com>
2022-12-27 14:35:51 -0500

    Attempt to make docs build pass

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,8 @@ templates_path = ["_templates"]
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = [".rst", ".ipynb"]
+# NOTE: `.rst` is the default suffix of sphinx, and nbsphinx will
+# automatically add support for `.ipynb` suffix.
 
 # do not execute cells
 nbsphinx_execute = "never"
@@ -106,7 +107,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
